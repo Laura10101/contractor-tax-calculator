bc78f0302d62087942b92be89c816b9021012c98
Laura10101 <>
2023-04-11 15:30:42 +0000

    Implement get_all_jurisdictions method

--- jurisdictions_api/services.py
+++ jurisdictions_api/services.py
@@ -1,1 +1,8 @@
+from .models import Jurisdiction
+
+
 # Create service method to retrieve all jurisdictions 
+def get_all_jurisdictions():
+    jurisdictions = Jurisdiction.objects.all()
+    return jurisdictions
+
