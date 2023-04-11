007da9db03ee7b904f3ad54e414accfe8ed6f8b1
Laura10101 <>
2023-04-11 12:45:21 +0000

    Add jurisdiction model class to jurisdiction api

--- jurisdictions_api/models.py
+++ jurisdictions_api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Jurisdiction(models.Model):
+    # No need to create ID attribute as Django will create automatically
+    # Info taken from Tim Nelson Models: Part 1 CI video at 5.30.
+
+    # Add name attribute and validation rules for attribute
+    name = models.CharField(max_length=50, null=False, blank=False)
