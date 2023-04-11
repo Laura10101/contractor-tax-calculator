e8bfcf7e82f763727a3943ad8e9c502c53778d3b
Laura10101 <>
2023-04-11 12:38:22 +0000

    Add codeanwhere host to ALLOWED HOSTS

--- contractor_tax_calculator/settings.py
+++ contractor_tax_calculator/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-z$-^w4kq@-iy^c^qr=)@q)1aw$uc!tmj&vb7osqpmi6pohnyx7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', '8000-laura10101-contractor-ta-sfibfuh38i.us2.codeanyapp.com']
 
 
 # Application definition
