52ba9bae2ab358fd577217ddda0fca5bdc9da6ae
Laura10101 <laurajanefindley@gmail.com>
2023-04-12 09:02:34 +0000

    Correctly serialize the jurisdictions to JSON data

--- jurisdictions_api/views.py
+++ jurisdictions_api/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.core.serializers import serialize
+import json
 from .services import get_all_jurisdictions
 
 # Create your views here.
 # Create the controller method to retrieve jurisdictions
 def get_jurisdictions(request):
+    # First, return all jurisdictions from the Django model
     jurisdictions = get_all_jurisdictions()
-    # Convert into a Python dictionary
-    # Django will translate this into a JSON object
-    response = { 'jurisdictions' : jurisdictions }
-    # Sending the HTTP response, containing the JSON data
-    # back to the client
-    return HttpResponse(response)
+    # Now to translate this into JSON data
+    serialized_data = serialize('json', jurisdictions.values())
+    # Package the JSON data up into a response object
+    response = { 'jurisdictions' : json.loads(serialized_data) }
+    # Sending the Json response back to the client
+    return JsonResponse(response)
