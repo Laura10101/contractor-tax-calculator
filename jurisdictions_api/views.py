59627b83ae322e6be0db6215ebe3e7320fe1b33c
Laura10101 <laurajanefindley@gmail.com>
2023-04-12 08:18:38 +0000

    Add comments to controller method

--- jurisdictions_api/views.py
+++ jurisdictions_api/views.py
@@ -5,5 +5,9 @@ from .services import get_all_jurisdictions
 # Create the controller method to retrieve jurisdictions
 def get_jurisdictions():
     jurisdictions = get_all_jurisdictions()
+    # Convert into a Python dictionary
+    # Django will translate this into a JSON object
     response = { 'jurisdictions' : jurisdictions }
+    # Sending the HTTP response, containing the JSON data
+    # back to the client
     return HttpResponse(response)
