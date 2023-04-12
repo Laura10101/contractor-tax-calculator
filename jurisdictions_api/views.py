e7a3ae3ffb8e8ea19626168349259e184e2fae53
Laura10101 <laurajanefindley@gmail.com>
2023-04-12 08:08:57 +0000

    Implement controller function to retrieve jurisdictions

--- jurisdictions_api/views.py
+++ jurisdictions_api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .services import get_all_jurisdictions
 
 # Create your views here.
+# Create the controller method to retrieve jurisdictions
+def get_jurisdictions():
+    jurisdictions = get_all_jurisdictions()
+    response = { 'jurisdictions' : jurisdictions }
+    return HttpResponse(response)
