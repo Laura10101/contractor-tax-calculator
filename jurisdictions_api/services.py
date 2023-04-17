531ed4271271ed66a2e814ab6dcc9ce6fe5ac5a0
Laura10101 <laurajanefindley@gmail.com>
2023-04-17 07:57:31 +0000

    Implement create jurisdiction method in service layer

--- jurisdictions_api/services.py
+++ jurisdictions_api/services.py
@@ -6,3 +6,10 @@ def get_all_jurisdictions():
     jurisdictions = Jurisdiction.objects.all()
     return jurisdictions
 
+def create_jurisdiction(name):
+    # Create new jurisdiction in the database 
+    new_jurisdiction = Jurisdiction.objects.create(name=name)
+    # Return ID of newly created jurisdiction
+    return new_jurisdiction.id
+
+
