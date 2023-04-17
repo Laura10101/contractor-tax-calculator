87252c73d5dcdb25e8219fcffe7dc3fbc722d06f
Laura10101 <laurajanefindley@gmail.com>
2023-04-17 08:08:23 +0000

    Create controller method to create new jurisdiction

--- jurisdictions_api/views.py
+++ jurisdictions_api/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.core.serializers import serialize
 import json
-from .services import get_all_jurisdictions
+from .services import get_all_jurisdictions, create_jurisdiction
 
 # Create your views here.
 # Create the controller method to retrieve jurisdictions
@@ -14,4 +14,15 @@ def get_jurisdictions(request):
     # Package the JSON data up into a response object
     response = { 'jurisdictions' : json.loads(serialized_data) }
     # Sending the Json response back to the client
+    return JsonResponse(response)
+
+# Create controller method to create new jurisdiction 
+def post_jurisdiction(request):
+    # Extract jurisdiction name from http request 
+    name = request.POST.get('name')
+    # Call services method to create new jurisdiction
+    jurisdiction_id = create_jurisdiction(name)
+    # Build JSON response including new jurisdiction ID
+    response = { 'jurisdiction_id' : jurisdiction_id }
+    # Send the response 
     return JsonResponse(response)
