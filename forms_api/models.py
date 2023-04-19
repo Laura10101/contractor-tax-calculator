d7052d82bba2593f06b46430b14a144782dda355
Laura10101 <laurajanefindley@gmail.com>
2023-04-19 10:30:07 +0000

    Create models for forms api

--- forms_api/models.py
+++ forms_api/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Form(models.Model):
+    pass
+
+class Question(models.Model):
+    pass
+
+class BooleanQuestion(models.Model):
+    pass
+
+class MultipleChoiceQuestion(models.Model):
+    pass
+
+class MultipleChoiceOption(models.Model):
+    pass
+
+class NumericQuestion(models.Model):
+    pass
+
+class NumericAnswerValidationRule(models.Model):
+    pass
