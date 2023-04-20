f1b73fed759063ff777fab9ea0d5a31d2cf44b74
Laura10101 <laurajanefindley@gmail.com>
2023-04-20 13:02:26 +0000

    Create interface for form service layer

---
+++ forms_api/services.py
@@ -0,0 +1,30 @@
+from .models import *
+
+# Create new method to get forms by jurisidiction ids 
+def get_forms_by_jurisdiction_id(ids):
+    pass
+
+# Create new method to create forms 
+def create_form():
+    pass
+
+# Create new method to update forms 
+def update_forms():
+    pass
+
+# Create new method to delete forms 
+def delete_forms(): 
+    pass 
+
+# Create new method to create questions 
+def create_question():
+    pass
+
+# Create new method to update questions 
+def update_question():
+    pass
+
+# Create new method to delete questions
+def delete_question():
+    pass 
+    
