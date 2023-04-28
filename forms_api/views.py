40274ba4ccd60dc2e02ed52af3caeb906b84769d
Laura10101 <laurajanefindley@gmail.com>
2023-04-28 10:26:23 +0000

    Implement get forms method on forms controller

--- forms_api/views.py
+++ forms_api/views.py
@@ -34,24 +34,76 @@ def handle_questions_request(request):
 # Controller methods
 # Process GET requests for forms 
 def get_form(request):
-    pass
+    # Extract relevant data from http request 
+    # Services method expects a list of jurisdiction ids
+    # List of jurisdiction ids will be provded as comma separated list in query string
+    # Get value of ids parameter from http request (query string)
+    id_string = request.GET['ids']
+    # Split string into array of strings 
+    id_strings = id_string.split(',')
+    # Parse the array of string values into an array integers 
+    id_ints = list(map(int, id_strings))
+    # Call apropriate services method
+    # Create variable to contain results of get method 
+    forms = get_forms_by_jurisdiction_ids(id_ints)
+    # Create response 
+    # Now to translate this into JSON data
+    serialized_data = serialize('json', forms)
+    # Package the JSON data up into a response object
+    response = { 'forms' : json.loads(serialized_data) }
+    # Sending the Json response back to the client
+    return response
 
 # Process CREATE requests for forms 
 def post_form(request):
     pass
+    # Extract relevant data from http request 
+
+    # Call apropriate services method
+
+    # Create response 
+
+    # Return response 
 
 # Process DELETE requests for forms
 def delete_form(request):
     pass
+    # Extract relevant data from http request 
+
+    # Call apropriate services method
+
+    # Create response 
+
+    # Return response 
 
 # Process CREATE requests for questions 
 def post_question(request):
     pass
+    # Extract relevant data from http request 
+
+    # Call apropriate services method
+
+    # Create response 
+
+    # Return response 
 
 # Process DELETE requests for questions 
 def delete_question(request):
     pass
+    # Extract relevant data from http request 
+    # Call apropriate services method
+
+    # Create response 
+
+    # Return response 
 
 # Process PUT requests for questions 
 def put_question(request):
     pass
+    # Extract relevant data from http request 
+
+    # Call apropriate services method
+
+    # Create response 
+
+    # Return response 
