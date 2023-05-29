aa4ebce24c0e161dcabb6c0fee5ea27c0f5bc062
Laura10101 <laurajanefindley@gmail.com>
2023-05-29 11:16:45 +0000

    Implement services methods for rules API

--- rules_api/services.py
+++ rules_api/services.py
@@ -3,50 +3,137 @@ from .models import *
 ### RULE SETS ###
 # Create rule set
 def create_ruleset(jurisdiction_id, tax_category_id):
-    pass
+    # Assign tax category id to new rule set 
+    tax_category = TaxCategory.objects.get(pk=tax_category_id)
+    # Create new ruleset in the database
+    new_ruleset = RuleSet.objects.create(
+        jurisdiction_id=jurisdiction_id,
+        tax_category=tax_category
+        )
+    # Return ID of newly created ruleset
+    return new_ruleset.id
 
 # Delete rule set 
 def delete_ruleset(id):
-    pass
+    RuleSet.objects.filter(pk__exact=id).delete()
 
 ### TAX CATEGORIES ###
 # Create tax category
 def create_tax_category(name):
-    pass
+    tax_category = TaxCategory.objects.create(name=name)
+    return tax_category.id
 
 # Delete tax category
 def delete_tax_category(id):
-    pass
+    TaxCategory.objects.filter(pk__exact=id).delete()
 
 ### FLAT RATE RULES ###
 # Create rule 
 def create_flat_rate_rule(name, ordinal, explainer, variable_name, tax_rate):
-    pass
+    flat_rate_rule = FlatRateRule.objects.create(
+        name=name, 
+        ordinal=ordinal, 
+        explainer=explainer, 
+        variable_name=variable_name,
+        flat_rate=tax_rate
+        )
+    return flat_rate_rule.id
 
 # Update rule 
 def update_flat_rate_rule(id, name, ordinal, explainer, variable_name, tax_rate):
-    pass 
+    FlatRateRule.objects.filter(pk__exact=id).update(
+        name=name,
+        ordinal=ordinal,
+        explainer=explainer,
+        variable_name=variable_name,
+        flat_rate=tax_rate        
+        )
 
 # Delete rule 
 def delete_flat_rate_rule(id):
-    pass
+    FlatRateRule.objects.filter(pk__exact=id).delete()
 
 ### TIERED RATE RULES ###
-def create_tiered_rate_rule(name, ordinal, explainer, variable_name, tiers):
-    pass
+def create_tiered_rate_rule(name, ordinal, explainer, variable_name):
+    tiered_rate_rule = TieredRateRule.objects.create(
+        name=name, 
+        ordinal=ordinal, 
+        explainer=explainer, 
+        variable_name=variable_name
+        )
+    return tiered_rate_rule.id
 
-def update_tiered_rate_rule(id, name, ordinal, explainer, variable_name, tax_rate, tiers):
-    pass
+def update_tiered_rate_rule(id, name, ordinal, explainer, variable_name):
+    TieredRateRule.objects.filter(pk__exact=id).update(
+        name=name,
+        ordinal=ordinal,
+        explainer=explainer,
+        variable_name=variable_name       
+        )
 
 def delete_tiered_rate_rule(id):
-    pass
+    TieredRateRule.objects.filter(pk__exact=id).delete()
+
+### RULE TIER ###
+# create a method to create rule tier
+def create_rule_tier(rule_id, min_value, max_value, ordinal, tier_rate):
+    # Get the rule from the database based on the rule id
+    rule = Rule.objects.get(pk=rule_id)
+    rule_tier = RuleTier.objects.create(
+        rule=rule,
+        min_value=min_value,
+        max_value=max_value,
+        ordinal=ordinal,
+        tier_rate=tier_rate
+    )
+    return rule_tier.id
+
+def update_rule_tier():
+    RuleTier.objects.filter(pk__exact=id).update(
+        min_value=min_value,
+        max_value=max_value,
+        ordinal=ordinal,
+        tier_rate=tier_rate   
+        )
+    
+def delete_rule_tier(id):
+    RuleTier.objects.filter(pk__exact=id).delete()
 
 ### SECONDARY TIERED RATE RULES ###
-def create_secondary_tiered_rate_rule(name, ordinal, explainer, variable_name, tiers):
-    pass
+def create_secondary_tiered_rate_rule(name, ordinal, explainer, variable_name):
+    secondary_tiered_rate_rule = SecondaryTieredRateRule.objects.create(
+        name=name, 
+        ordinal=ordinal, 
+        explainer=explainer, 
+        variable_name=variable_name
+        )
+    return secondary_tiered_rate_rule.id
 
-def update_secondary_tiered_rate_rule(id, name, ordinal, explainer, variable_name, tax_rate, tiers):
-    pass
+def update_secondary_tiered_rate_rule(id, name, ordinal, explainer, variable_name, tax_rate):
+    SecondaryTieredRateRule.objects.filter(pk__exact=id).update(
+        name=name, 
+        ordinal=ordinal, 
+        explainer=explainer, 
+        variable_name=variable_name   
+        )
 
 def delete_secondary_tiered_rate_rule(id):
-    pass
+    SecondaryTieredRateRule.objects.filter(pk__exact=id).delete()
+
+### SECONDARY RULE TIER ###
+def create_secondary_rule_tier(primary_tier_id, tier_rate):
+    # Get the primary rule object from the database based on the rule tier id
+    primary_tier = RuleTier.objects.get(pk=primary_tier_id)
+    secondary_rule_tier = SecondaryRuleTier.objects.create(
+        primary_tier=primary_tier,
+        tier_rate=tier_rate
+    )
+    return rule_tier.id
+
+def update_secondary_rule_tier(id, tier_rate):
+    SecondaryRuleTier.objects.filter(pk__exact=id).update(
+        tier_rate=tier_rate  
+        )
+
+def delete_secondary_rule_tier(id):
+    SecondaryRuleTier.objects.filter(pk__exact=id).delete()
