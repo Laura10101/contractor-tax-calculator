ec4d5d1fed065f9070bad68788f4ca48d9607fe7
Laura10101 <laurajanefindley@gmail.com>
2023-06-05 13:31:02 +0000

    Add Orders to admin

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from .models import Order
 # Register your models here.
+class OrderAdmin(admin.ModelAdmin):
+    readonly_fields = ('order_number', 'date',)
+    
+    fields = ('order_number', 'date', 'full_name', 'email',
+              'phone_number', 'country', 'postcode', 'tonw_or_city',
+              'street_address1', 'street_address2', 'county', 'order_total',)
+
+    list_display = ('order_number', 'date', 'full_name', 'order_total')
+
+    ordering = ('-date',)
+
+admin.sites.register(Order, OrderAdmin)
+
