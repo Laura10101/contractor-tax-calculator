a67fec3adbae7a1c125fc46a7f3d85f2d1331482
Laura10101 <laurajanefindley@gmail.com>
2023-06-07 10:42:52 +0000

    Add imports to subscription API test files

---
+++ subscriptions_api/test_services.py
@@ -0,0 +1,5 @@
+from django.db import IntegrityError
+from django.core.exceptions import ValidationError
+from .models import *
+from .services import *
+import pytest
---
+++ subscriptions_api/test_views.py
@@ -0,0 +1,7 @@
+from django.db import IntegrityError
+from django.core.exceptions import ValidationError
+from rest_framework.test import APIClient
+from .models import *
+from .services import *
+from .views import *
+import pytest
