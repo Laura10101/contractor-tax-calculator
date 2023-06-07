0029d432933028aeca4b54950410a3be6cf02cd0
Laura10101 <laurajanefindley@gmail.com>
2023-06-07 10:49:53 +0000

    Define test cases for subscription API

--- subscriptions_api/test_services.py
+++ subscriptions_api/test_services.py
@@ -3,3 +3,51 @@ from django.core.exceptions import ValidationError
 from .models import *
 from .services import *
 import pytest
+
+# Test creating a subscription
+def test_create_subscription_with_null_data():
+    pass
+
+def test_create_subscription_with_null_user_id():
+    pass
+
+def test_create_subscription_with_nonexistent_user_id():
+    pass
+
+def test_create_subscription_with_null_months():
+    pass
+
+def test_create_subscription_with_negative_months():
+    pass
+
+def test_create_valid_subscription():
+    pass
+
+# Test updating a subscription
+def test_update_subscription_with_null_user_id():
+    pass
+
+def test_update_subscription_with_nonexistent_user_id():
+    pass
+
+def test_update_subscription_with_null_months():
+    pass
+
+def test_update_subscription_with_negative_months():
+    pass
+
+def test_valid_subscription_update():
+    pass
+
+# Test getting the subscription status
+def test_get_status_for_nonexistent_user_id():
+    pass
+
+def test_get_status_where_no_subscription_exists():
+    pass
+
+def test_get_status_where_subscription_expired():
+    pass
+
+def test_get_status_where_subscription_active():
+    pass
