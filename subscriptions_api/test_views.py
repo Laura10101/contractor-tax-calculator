1d84ccfd55870ddc7f39af3c298ded062a2aa27f
Laura10101 <laurajanefindley@gmail.com>
2023-06-07 10:53:56 +0000

    Define view tests for subscription API

--- subscriptions_api/test_views.py
+++ subscriptions_api/test_views.py
@@ -5,3 +5,75 @@ from .models import *
 from .services import *
 from .views import *
 import pytest
+
+from django.db import IntegrityError
+from django.core.exceptions import ValidationError
+from .models import *
+from .services import *
+import pytest
+
+# Test creating a subscription
+def test_post_subscription_with_no_data():
+    pass
+
+def test_post_subscription_with_null_data():
+    pass
+
+def test_post_subscription_with_no_user_id():
+    pass
+
+def test_post_subscription_with_null_user_id():
+    pass
+
+def test_post_subscription_with_nonexistent_user_id():
+    pass
+
+def test_post_subscription_with_no_months():
+    pass
+
+def test_post_subscription_with_null_months():
+    pass
+
+def test_post_subscription_with_negative_months():
+    pass
+
+def test_post_valid_subscription():
+    pass
+
+# Test updating a subscription
+def test_patch_subscription_with_no_user_id():
+    pass
+
+def test_patch_subscription_with_null_user_id():
+    pass
+
+def test_patch_subscription_with_nonexistent_user_id():
+    pass
+
+def test_patch_subscription_with_no_months():
+    pass
+
+def test_patch_subscription_with_null_months():
+    pass
+
+def test_patch_subscription_with_negative_months():
+    pass
+
+def test_patch_subscription_update():
+    pass
+
+# Test getting the subscription status
+def test_get_status_without_user_id():
+    pass
+
+def test_get_status_for_nonexistent_user_id():
+    pass
+
+def test_get_status_where_no_subscription_exists():
+    pass
+
+def test_get_status_where_subscription_expired():
+    pass
+
+def test_get_status_where_subscription_active():
+    pass
