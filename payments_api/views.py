b62d366a2501fb4c46c3596ac7e2bfea66e96ee9
Laura10101 <laurajanefindley@gmail.com>
2023-06-11 12:12:00 +0000

    Implement APIView to receive Stripe webhooks in Payments API

--- payments_api/views.py
+++ payments_api/views.py
@@ -1,11 +1,13 @@
+from django.conf import settings
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.core.serializers import serialize
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.views import APIView
 from rest_framework.response import Response
-import json
 from .services import *
+import json
+import stripe
 
 # Create a function to validate that a request contains
 # the required attributes, so the API views can use it
@@ -78,5 +80,60 @@ class PaymentDetail(APIView):
         return Response(response)
         
 class StripeWebhooksList(APIView):
+    # Create function to handle webhooks from Stripe
     def post(self, request):
-        pass
+        # Validate the webhook signature for security purposes
+        # This is taken from the Boutique ADO video by Tim Nelson
+        wh_secret = settings.STRIPE_WH_SECRET
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+
+        try:
+            event = stripe.Webhook.construct_event(
+                request.data, sig_header, wh_secret
+            )
+        except ValueError as e:
+            # Invalid payload
+            return HttpResponse(content=e, status=400)
+        except stripe.error.SignatureVerificationError as e:
+            # Invalid signature
+            return HttpResponse(content=e, status=400)
+        except Exception as e:
+            return HttpResponse(content=e, status=400)
+
+        # Does it contain all of the attributes expected?
+        if 'type' not in request.data.keys():
+            return Response(
+                { 'error' : 'Invalid request. Please supply all required attributes.' },
+                status=status.HTTP_400_BAD_REQUEST
+                )
+
+        if 'id' not in request.data['data']['object'].keys():
+            return Response(
+                { 'error' : 'Invalid request. Please supply all required attributes.' },
+                status=status.HTTP_400_BAD_REQUEST
+                )
+
+        if 'status' not in request.data['data']['object'].keys():
+            return Response(
+                { 'error' : 'Invalid request. Please supply all required attributes.' },
+                status=status.HTTP_400_BAD_REQUEST
+                )
+
+        # Extract the attributes from the webhook request
+        event_type = request.data['type']
+        stripe_pid = request.data['data']['object']['id']
+        status = request.data['data']['object']['status']
+        # Call the apropriate services function based on the event type
+        # Create switch statmeent to determine which event type has occurred 
+        match event_type:
+            case 'payment_intent.canceled':
+                fail_payment(stripe_pid, status)
+            case 'payment_intent.payment_failed':
+                fail_payment(stripe_pid, status)
+            case 'payment_intent.requires_action':
+                fail_payment(stripe_pid, status)
+            case 'payment_intent.succeeded':
+                complete_payment(stripe_pid)
+        # Return response 
+        return HttpResponse((f'Webhook successfully processed: {event["type"]}'), status=200)
