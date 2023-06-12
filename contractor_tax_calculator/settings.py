7f1419f7f63e3251d9d182abf7f428f2bbf5f115
Laura10101 <laurajanefindley@gmail.com>
2023-06-12 19:08:46 +0000

    Prepare settings.py for deployment

--- contractor_tax_calculator/settings.py
+++ contractor_tax_calculator/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-z$-^w4kq@-iy^c^qr=)@q)1aw$uc!tmj&vb7osqpmi6pohnyx7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEVELOPMENT', False)
 
-ALLOWED_HOSTS = ['8000-laura10101-contractorta-x36vvy68pn6.ws-eu99.gitpod.io']
+ALLOWED_HOSTS = ['8000-laura10101-contractorta-x36vvy68pn6.ws-eu99.gitpod.io', 'contractor-tax-calculator.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -120,12 +121,17 @@ WSGI_APPLICATION = 'contractor_tax_calculator.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
