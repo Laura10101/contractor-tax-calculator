6152b37850d60eab6bbfae91acd6cd79b4c6de92
Laura10101 <laurajanefindley@gmail.com>
2023-06-12 19:45:33 +0000

    Default anonymous users to having no subscription

--- subscription/context_processors.py
+++ subscription/context_processors.py
@@ -5,10 +5,14 @@ import json
 
 def user_has_subscription(request):
     url = 'https://8000-laura10101-contractorta-x36vvy68pn6.ws-eu99.gitpod.io/api/subscriptions/status/'
-    user_id = request.user.id
-    response = requests.get(url + '?user_id=' + str(user_id))
-    data = json.loads(response.text)
-    user_has_subscription = data['has_active_subscription']
+    print('Checking the user: ' + str(request.user))
+    user_has_subscription = False
+    if not str(request.user) == 'AnonymousUser':
+        user_id = request.user.id
+        response = requests.get(url + '?user_id=' + str(user_id))
+        print(response)
+        data = json.loads(response.text)
+        user_has_subscription = data['has_active_subscription']
     return { 
         'user_has_subscription' : user_has_subscription,
         'subscription_exempt_paths': settings.SUBSCRIPTION_EXEMPT_PATHS
