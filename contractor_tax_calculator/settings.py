aefb481a2b3a464adcb712fad1826f86c16b41a6
Laura10101 <laurajanefindley@gmail.com>
2023-06-12 20:39:16 +0000

    Add cache control to settings

--- contractor_tax_calculator/settings.py
+++ contractor_tax_calculator/settings.py
@@ -189,6 +189,10 @@ STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 
 if 'USE_AWS' in os.environ:
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
     AWS_STORAGE_BUCKET_NAME = 'contractor-tax-calculator'
     AWS_S3_REGION_NAME = 'eu-west-2'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
