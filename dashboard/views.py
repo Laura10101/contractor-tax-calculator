5caf2aea28197424c154c0ec1aed0aea27adc2d6
Laura10101 <laurajanefindley@gmail.com>
2023-06-12 11:20:02 +0000

    Implement dashboard view function

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def dashboard(request):
+    template = 'dashboard/dashboard.html'
+    context = {
+    }
+
+    return render(request, template, context)
