116c36465fccc8cdf031468ffc6bfcaa4bfaeb63
Laura10101 <laurajanefindley@gmail.com>
2023-06-12 15:27:20 +0000

    Implement context processor to check subscription status

--- subscription/context_processors.py
+++ subscription/context_processors.py
@@ -1,4 +1,13 @@
 # Context processor to check user subscription status 
+import requests
+import json
 
 def user_has_subscription(request):
-    pass
+    url = 'https://8000-laura10101-contractorta-x36vvy68pn6.ws-eu99.gitpod.io/api/subscriptions/status/'
+    user_id = request.user.id
+    response = requests.get(url + '?user_id=' + str(user_id))
+    data = json.loads(response.text)
+    user_has_subscription = data['has_active_subscription']
+    return { 
+        'user_has_subscription' : user_has_subscription
+    }
