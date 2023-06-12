e78c44ba5096863fc7c918800b883a09c7eaccde
Laura10101 <laurajanefindley@gmail.com>
2023-06-12 11:36:40 +0000

    Create subscription views function

--- subscription/views.py
+++ subscription/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def subscription(request):
+    template = 'subscription/subscription.html'
+    context = {
+    }
+
+    return render(request, template, context)
