023e1517c929cb0cd5004f3376b3dd8a9187e29c
Laura10101 <laurajanefindley@gmail.com>
2023-06-12 19:14:20 +0000

    Removed secret key and set debug

--- contractor_tax_calculator/settings.py
+++ contractor_tax_calculator/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z$-^w4kq@-iy^c^qr=)@q)1aw$uc!tmj&vb7osqpmi6pohnyx7'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEVELOPMENT', False)
