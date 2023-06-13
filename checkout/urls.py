eb16aebe7f6ffb43299dec5b60abd33c436ff976
Laura10101 <laurajanefindley@gmail.com>
2023-06-13 14:50:36 +0000

    Add checkout status url

--- checkout/urls.py
+++ checkout/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.checkout, name='checkout')
+    path('', views.checkout, name='checkout'),
+    path('status/', views.checkout_status, name='checkout_status')
 ]
