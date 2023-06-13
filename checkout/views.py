c982b5476daad8bd0d961093ca29ce52de5132cd
Laura10101 <laurajanefindley@gmail.com>
2023-06-13 14:40:49 +0000

    Confirm payment or check payment status when accessing checkout status page

--- checkout/views.py
+++ checkout/views.py
@@ -50,3 +50,44 @@ def checkout(request):
         'payment_id': payment_id
     }
     return render(request, template, context)
+
+# Create function for views on checkout status page: talk to payment API to get payment status
+def checkout_status(request):
+    # Define template
+    template = 'checkout/checkout_status.html'
+    base_url = request.scheme + '://' + request.get_host() + '/api/payments/'
+    # If request method is POST, confirm payment in payments API
+    if request.method == 'POST':
+        payment_id = request.POST['payment_id']
+        url = base_url + str(payment_id) + '/'
+        data = {
+            'billing_street_1': request.POST['street_address1'],
+            'billing_street_2': request.POST['street_address2'],
+            'town_or_city': request.POST['town_or_city'],
+            'county': request.POST['county'],
+            'country': request.POST['country'],
+            'postcode': request.POST['postcode'],
+            'stripe_card_id': request.POST['payment_method_id']
+        }
+        print("Confirming payment at URL: " + url)
+        response = requests.patch(url, json=data)
+        print('Confirm payment response: ' + str(response))
+        data = json.loads(response.text)
+        payment_status = 'pending'
+    # If request method is GET, get payment status from payments API
+    elif request.method == 'GET':
+        payment_id = request.GET['payment_id']
+        url = base_url + str(payment_id) + '/status/'
+        response = requests.get(url)
+        data = json.loads(response.text)
+        payment_status = data['status']
+        failure_reason = data['failure_reason']
+    # Build context 
+    context = { 
+        'payment_id': payment_id,
+        'payment_status': payment_status,
+        'failure_reason': failure_reason,
+        'payment_pending': pamyent_pending,
+    }
+    # Render template and return 
+    return render(request, template, context)
