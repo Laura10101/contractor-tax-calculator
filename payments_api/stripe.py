9b596a477757a40e81b27b4b5166bc338b10dada
Laura10101 <laurajanefindley@gmail.com>
2023-06-13 11:19:18 +0000

    Convert amount in gbp for Stripe

--- payments_api/stripe.py
+++ payments_api/stripe.py
@@ -7,13 +7,18 @@ stripe_public_key = settings.STRIPE_PUBLIC_KEY
 stripe_secret_key = settings.STRIPE_SECRET_KEY
 
 # Create payment intention for Stripe 
+# Anount is in GBP
 def create_stripe_payment_intention(amount, currency):
     # Then create payment intent
     # This code is from the CI video by Tim Nelson 
     stripe.api_key = stripe_secret_key
     intent = stripe.PaymentIntent.create(
-        amount=stripe_total,
-        currency=settings.STRIPE_CURRENCY,
+        # From Stripe documentation:
+        # 'Amount intended to be collected by this PaymentIntent.
+        #  A positive integer representing how much to charge in the smallest currency unit'
+        # https://stripe.com/docs/api/payment_intents/create
+        amount=int(amount * 100),
+        currency=currency,
     )
     return intent.id, intent.client_secret
 
