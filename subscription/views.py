edc1fb91c79ffe00a2093dd4c00bc834b7d68552
Laura10101 <laurajanefindley@gmail.com>
2023-06-18 17:38:08 +0000

    Generate subscription page dynamically from subscription options API

--- subscription/views.py
+++ subscription/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
+import requests
+import json
 
 # Create your views here.
 def subscription(request):
+    # List subscription options
+    base_url = request.scheme + '://' + request.get_host()
+    url = base_url + '/api/subscriptions/options'
+
+    response = requests.get(url)
+    data = json.loads(response.text)
+
     template = 'subscription/subscription.html'
     context = {
+        'subscription_options': data['subscription_options']
     }
 
     return render(request, template, context)
