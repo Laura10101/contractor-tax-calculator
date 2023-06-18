7834994e385adf5ee0892a51d4e170af9027cf5f
Laura10101 <laurajanefindley@gmail.com>
2023-06-18 17:10:00 +0000

    Make subscription option FK nullable and create migrations

--- subscriptions_api/models.py
+++ subscriptions_api/models.py
@@ -3,16 +3,20 @@ from datetime import date, timedelta
 
 # Create your models here.
 
+class SubscriptionOption(models.Model):
+    subscription_months = models.IntegerField()
+    subscription_price = models.DecimalField(decimal_places=2, null=False, blank=False, max_digits=6)
+    is_active = models.BooleanField(null=False, blank=False)
+
 class Subscription(models.Model):
     user_id = models.IntegerField()
-    subscription_option = models.ForeignKey(SubscriptionOption, on_delete=models.CASCADE)
+    subscription_option = models.ForeignKey(
+        SubscriptionOption,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True)
     start_date = models.DateTimeField(auto_now_add=True, null=False, blank=False)
 
     def is_active(self):
         expiry_date = F(self.start_date) + relativedelta(months=self.subscription_months)
         return date.today() <= expiry_date
-
-class SubscriptionOption(models.Model):
-    subscription_months = models.IntegerField()
-    subscription_price = models.DecimalField(decimal_places=2, null=False, blank=False, max_digits=6)
-    is_active = models.BooleanField(null=False, blank=False)
