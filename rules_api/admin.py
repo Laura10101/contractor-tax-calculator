1516d04f23ac84be558a104938a596ac7acb3295
Laura10101 <laurajanefindley@gmail.com>
2023-06-19 17:30:34 +0000

    Register rules API models with admin app

--- rules_api/admin.py
+++ rules_api/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import TaxCategory, RuleSet, FlatRateRule, TieredRateRule, SecondaryTieredRateRule, \
+    RuleTier, SecondaryRuleTier
 
 # Register your models here.
+admin.site.register(TaxCategory)
+admin.site.register(RuleSet)
+admin.site.register(FlatRateRule)
+admin.site.register(TieredRateRule)
+admin.site.register(SecondaryTieredRateRule)
+admin.site.register(RuleTier)
+admin.site.register(SecondaryRuleTier)
