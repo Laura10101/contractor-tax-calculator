ab79ee89448086084769b5c35f234a50a70d1bfa
Laura10101 <laurajanefindley@gmail.com>
2023-06-19 17:11:45 +0000

    Implement select jurisdictions view

--- calculations/views.py
+++ calculations/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from django.urls import reverse
+
+import requests
+import json
 
 # Create your views here.
 def select_jurisdictions(request):
-    pass
+    template = 'calculations/select_jurisdictions.html'
+    url = request.build_absolute_uri(reverse('jurisdictions'))
+    response = requests.get(url)
+    print(response.text)
+    data = json.loads(response.text)
+    print('Jurisdictions: ' + str(data['jurisdictions']))
+
+    context = {
+        'jurisdictions': data['jurisdictions']
+    }
+    return render(request, template, context)
