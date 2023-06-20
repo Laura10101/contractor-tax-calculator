13ac90476e5b8361e8e85cbbc81cfa3808751528
Laura10101 <laurajanefindley@gmail.com>
2023-06-20 07:17:10 +0000

    Define custom __str__ for subscription models

--- subscriptions_api/models.py
+++ subscriptions_api/models.py
@@ -14,6 +14,9 @@ class SubscriptionOption(models.Model):
     def total(self):
         return round(float(self.subscription_price) + self.vat(), 2)
 
+    def __str__(self):
+        return self.subscription_months + ' month extension (£' + str(self.subscription_price) + ')'
+
 class Subscription(models.Model):
     user_id = models.IntegerField()
     subscription_option = models.ForeignKey(
@@ -27,3 +30,6 @@ class Subscription(models.Model):
         subscription_months = self.subscription_option.subscription_months
         expiry_date = F(self.start_date) + relativedelta(months=subscription_months)
         return date.today() <= expiry_date
+
+    def __str__(self):
+        return 'Subscription for user ' + str(self.user_id)
