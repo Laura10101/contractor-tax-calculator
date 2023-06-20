231ae63a35c0f7886e5c103ccc51604252062a88
Laura10101 <laurajanefindley@gmail.com>
2023-06-20 06:55:28 +0000

    Define custom str functions for all form models

--- forms_api/models.py
+++ forms_api/models.py
@@ -1,12 +1,17 @@
 from django.db import models
 from polymorphic.models import PolymorphicModel
 
+from jurisdiction_api.models import Jurisdiction
+
 # Create your models here.
 class Form(models.Model):
     # Questions - one to many so the code for this is in the Question class as that is where
     # the foreign key needs to be
     # Jurisdiction ID 
     jurisdiction_id = models.IntegerField()
+
+    def __str__(self):
+        return Jurisdiction.objects.get(pk=self.jurisdiction_id).name + '  Form'
 
 class Question(PolymorphicModel):
     # Create foreign key for Form/Question relationship 
@@ -19,6 +24,9 @@ class Question(PolymorphicModel):
     explainer = models.CharField(max_length=255, null=False, blank=False)
     # Is question mandatory or not
     is_mandatory = models.BooleanField()
+
+    def __str__(self):
+        return str(self.form) + ': ' + text
 
 class BooleanQuestion(Question):
     pass
@@ -39,6 +47,9 @@ class MultipleChoiceOption(models.Model):
     text = models.CharField(max_length=255, null=False, blank=False)
     # Explanatory notes for each option 
     explainer = models.CharField(max_length=255, null=False, blank=False)
+
+    def __str__(self):
+        return str(self.question) + ' - ' + self.text
 
 class NumericQuestion(Question):
     # Is integer: boolean
