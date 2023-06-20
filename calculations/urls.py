49c254f80564ee1aad11407a042761a9d1b37cba
Laura10101 <laurajanefindley@gmail.com>
2023-06-20 14:13:33 +0000

    Create calculation form template and view

--- calculations/urls.py
+++ calculations/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('jurisdictions/select', views.select_jurisdictions, name='select_jurisdictions'),
+    path('form', views.display_form, name='display_form'),
 ]
--- calculations/views.py
+++ calculations/views.py
@@ -5,6 +5,7 @@ import requests
 import json
 
 # Create your views here.
+# Create view for select jurisdictions form 
 def select_jurisdictions(request):
     template = 'calculations/select_jurisdictions.html'
     url = request.build_absolute_uri(reverse('jurisdictions'))
@@ -17,3 +18,12 @@ def select_jurisdictions(request):
         'jurisdictions': data['jurisdictions']
     }
     return render(request, template, context)
+
+# Create view for displaying financial information form
+# This will use the calculation form template 
+def display_form(request):
+    template = 'calculations/calculation_form.html'
+    # Context is used to pass data into the template 
+    context = { }
+    return render(request, template, context)
+    
