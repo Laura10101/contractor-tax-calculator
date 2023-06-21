d09aa7877d6667d62dfdcf1d8bf8877fe35b6fe4
Laura10101 <laurajanefindley@gmail.com>
2023-06-21 18:40:32 +0000

    Correctly display jurisdictions in the calculation form accordion

--- calculations/helpers.py
+++ calculations/helpers.py
@@ -11,7 +11,7 @@ def get_jurisdictions_by_ids(url, ids):
         raise Exception('Received status code of ' + str(response.status_code) + ' when retrieving jurisdictions.')
 
     data = json.loads(response.text)
-    return data
+    return data['jurisdictions']
 
 # Create helper function to return form data based on a list of jurisdiction IDs 
 def get_forms_by_jurisdiction_ids(url, jurisdiction_ids):
@@ -22,4 +22,4 @@ def get_forms_by_jurisdiction_ids(url, jurisdiction_ids):
         raise Exception('Received status code of ' + str(response.status_code) + ' when retrieving forms.')
 
     data = json.loads(response.text)
-    return data
+    return data['forms']
--- calculations/views.py
+++ calculations/views.py
@@ -48,16 +48,19 @@ def display_form(request):
     forms_url = request.build_absolute_uri(reverse('forms'))
     # Generate jurisdiction id list 
     selected_jurisdictions_ids = request.POST.getlist("jurisdictions")
-    ids = []
+    # Always include the global jurisdiction as the first in the list
+    ids = [1]
     for jurisdiction_id in selected_jurisdictions_ids:
         ids.append(int(jurisdiction_id))
     # Use helper function to get jurisdiction data 
     jurisdictions = get_jurisdictions_by_ids(jurisdictions_url, ids)
+    print('Jurisdictions data: ' + str(jurisdictions))
     # Use helper function to get forms data
     forms = get_forms_by_jurisdiction_ids(forms_url, ids)
+    print('Forms data: ' + str(forms))
     # Context is used to pass data into the template 
     context = { 
-        'jurisdiction': jurisdictions,
+        'jurisdictions': jurisdictions,
         'forms': forms,
     }
     return render(request, template, context)
