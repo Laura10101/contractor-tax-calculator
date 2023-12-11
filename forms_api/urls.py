9d9588100c59182559127a18164a050d03d27dac
Laura10101 <laurajanefindley@gmail.com>
2023-12-11 09:06:05 +0000

    Move forms API urls to local urls.py

---
+++ forms_api/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('<int:form_pk>/questions/<int:pk>/', FormQuestionsDetail.as_view()),
+    path('<int:form_pk>/questions/', FormQuestionList.as_view()),
+    path('<int:pk>/', FormDetail.as_view()),
+    path('', FormsList.as_view(), name='forms'),
+]
+
+    
