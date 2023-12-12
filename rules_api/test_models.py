0e34ac494ad67caa00acf281b9295462938b23a4
James Naish <james.naish@ground-control.co.uk>
2023-12-12 07:49:52 +0000

    Create flat rate rule with fully instantiated ruleset

--- rules_api/test_models.py
+++ rules_api/test_models.py
@@ -1,4 +1,5 @@
 from .models import *
+from jurisdictions_api.models import Jurisdiction
 import math
 import pytest
 
@@ -7,135 +8,163 @@ import pytest
 # This validation is tested in test_services.py and test_views.py respectively.
 
 # Helper functions
-def create_empty_results_table():
-    return []
+def create_mock_test_calculation_result():
+    tax_calculation_result = TaxCalculationResult.objects.create(username='bob')
+    return TaxRuleSetResult.objects.create(
+        tax_calculation_result=tax_calculation_result,
+        jurisdiction_id=1,
+        tax_category_id=1,
+        tax_category_name='Test',
+        ordinal=1
+    )
 
-def create_variable_table(primary_income, secondary_income=None):
-    table = {
-        'primary_income': primary_income
+def create_mock_variable_table(salary=9000, dividends=50000, company_profit=100000):
+    return {
+        'salary': salary,
+        'dividends': dividends,
+        'company_profit': company_profit
     }
 
-    if secondary_income is not None:
-        table['secondary_income'] = secondary_income
-    
-    return table
+def create_mock_ruleset():
+    jurisdiction = Jurisdiction.objects.create(name='Test Jurisdiction')
+    tax_category = TaxCategory.objects.create(name='Test Category')
+    ruleset = RuleSet.objects.create(
+        jurisdiction_id=jurisdiction.id,
+        tax_category=tax_category,
+        ordinal=1
+    )
+    return ruleset
 
 # Test flat rate calculations
+@pytest.mark.django_db
 def test_flat_rate_calculate():
-    primary_income = 70000
-    flat_rate = 10
-    rule = FlatRateRule(
-        variable_name='primary_income',
-        flat_rate = flat_rate
+    flat_rate = 20
+    rule = FlatRateRule.objects.create(
+        variable_name='company_profit',
+        flat_rate = flat_rate,
+        ruleset=create_mock_ruleset(),
+        name='Flat Rate Test',
+        ordinal=1
     )
-    variables = create_variable_table(primary_income)
-    results = create_empty_results_table()
+    variables = create_mock_variable_table()
+    results = create_mock_test_calculation_result()
     rule.calculate(variables, results)
     assert len(results) == 1
     assert results[0] is not None
-    assert results[0]['tax_subtotal'] == primary_income * (flat_rate / 100)
+    assert results[0]['tax_subtotal'] == variables['company_profit'] * (flat_rate / 100)
 
 
 # Test rule tier calculations
+@pytest.mark.django_db
 def test_rule_tier_calculate_where_income_below_boundary():
     primary_income = 9999
     tier = RuleTier(min_value=10000, max_value=45000, tier_rate=10)
-    results = create_empty_results_table()
+    results = create_mock_test_calculation_result()
     tier.calculate(primary_income, results)
     assert len(results) == 0
 
+@pytest.mark.django_db
 def test_rule_tier_calculate_where_income_on_lower_boundary():
     primary_income = 10000
     tier = RuleTier(min_value=10000, max_value=45000, tier_rate=10)
-    results = create_empty_results_table()
+    results = create_mock_test_calculation_result()
     tier.calculate(primary_income, results)
     assert len(results) == 1
     assert results[0] is not None
     assert results[0]['tax_subtotal'] == round((primary_income - tier.min_value) * (tier.tier_rate / 100), 2)
 
+@pytest.mark.django_db
 def test_rule_tier_calculate_where_income_within_boundaries():
     primary_income = 25000
     tier = RuleTier(min_value=10000, max_value=45000, tier_rate=10)
-    results = create_empty_results_table()
+    results = create_mock_test_calculation_result()
     tier.calculate(primary_income, results)
     assert len(results) == 1
     assert results[0] is not None
     assert results[0]['tax_subtotal'] == round((primary_income - tier.min_value) * (tier.tier_rate / 100), 2)
 
+@pytest.mark.django_db
 def test_rule_tier_calculate_where_income_on_upper_boundary():
     primary_income = 45000
     tier = RuleTier(min_value=10000, max_value=45000, tier_rate=10)
-    results = create_empty_results_table()
+    results = create_mock_test_calculation_result()
     tier.calculate(primary_income, results)
     assert len(results) == 1
     assert results[0] is not None
     assert results[0]['tax_subtotal'] == round((tier.max_value - tier.min_value) * (tier.tier_rate / 100), 2)
 
+@pytest.mark.django_db
 def test_rule_tier_calculate_where_income_above_upper_boundary():
     primary_income = 45001
     tier = RuleTier(min_value=10000, max_value=45000, tier_rate=10)
-    results = create_empty_results_table()
+    results = create_mock_test_calculation_result()
     tier.calculate(primary_income, results)
     assert len(results) == 1
     assert results[0] is not None
     assert results[0]['tax_subtotal'] == round((tier.max_value - tier.min_value) * (tier.tier_rate / 100), 2)
 
+@pytest.mark.django_db
 def test_rule_tier_calculate_where_no_upper_boundary_and_income_above_lower_boundary():
     primary_income = 45001
     tier = RuleTier(min_value=10000, tier_rate=10)
-    results = create_empty_results_table()
+    results = create_mock_test_calculation_result()
     tier.calculate(primary_income, results)
     assert len(results) == 1
     assert results[0] is not None
     assert results[0]['tax_subtotal'] == round((primary_income - tier.min_value) * (tier.tier_rate / 100), 2)
 
 # Test secondary rule tier calculations
+@pytest.mark.django_db
 def test_secondary_tier_calculate_where_total_income_below_lower_boundary():
     primary_income = 9999
     secondary_income = 0
     tier = RuleTier(min_value=10000, max_value=45000, tier_rate=10)
     secondary_tier = SecondaryRuleTier(primary_tier=tier, tier_rate=10)
-    results = create_empty_results_table()
+    results = create_mock_test_calculation_result()
     secondary_tier.calculate(secondary_income, primary_income, results)
     assert len(results) == 0
 
+@pytest.mark.django_db
 def test_secondary_tier_calculate_where_primary_income_on_lower_boundary_and_no_secondary_income():
     primary_income = 10000
     secondary_income = 0
     tier = RuleTier(min_value=10000, max_value=45000, tier_rate=10)
     secondary_tier = SecondaryRuleTier(primary_tier=tier, tier_rate=10)
-    results = create_empty_results_table()
+    results = create_mock_test_calculation_result()
     secondary_tier.calculate(secondary_income, primary_income, results)
     assert len(results) == 0
 
+@pytest.mark.django_db
 def test_secondary_tier_calculate_where_primary_income_on_lower_boundary_and_total_within_boundaries():
     primary_income = 10000
     secondary_income = 10000
     tier = RuleTier(min_value=10000, max_value=45000, tier_rate=10)
     secondary_tier = SecondaryRuleTier(primary_tier=tier, tier_rate=10)
-    results = create_empty_results_table()
+    results = create_mock_test_calculation_result()
     secondary_tier.calculate(secondary_income, primary_income, results)
     assert len(results) == 1
     assert results[0] is not None
     assert results[0]['tax_subtotal'] == round(secondary_income * (tier.tier_rate / 100), 2)
 
+@pytest.mark.django_db
 def test_secondary_tier_calculate_where_primary_income_and_total_within_boundaries():
     primary_income = 20000
     secondary_income = 10000
     tier = RuleTier(min_value=10000, max_value=45000, tier_rate=10)
     secondary_tier = SecondaryRuleTier(primary_tier=tier, tier_rate=10)
-    results = create_empty_results_table()
+    results = create_mock_test_calculation_result()
     secondary_tier.calculate(secondary_income, primary_income, results)
     assert len(results) == 1
     assert results[0] is not None
     assert results[0]['tax_subtotal'] == round(secondary_income * (tier.tier_rate / 100), 2)
 
+@pytest.mark.django_db
 def test_secondary_tier_calculate_where_primary_income_within_boundaries_and_total_exceeds():
     primary_income = 35000
     secondary_income = 20000
     tier = RuleTier(min_value=10000, max_value=45000, tier_rate=10)
     secondary_tier = SecondaryRuleTier(primary_tier=tier, tier_rate=10)
-    results = create_empty_results_table()
+    results = create_mock_test_calculation_result()
     secondary_tier.calculate(secondary_income, primary_income, results)
 
     tier_amount = tier.max_value - tier.min_value
@@ -145,12 +174,13 @@ def test_secondary_tier_calculate_where_primary_income_within_boundaries_and_tot
     assert results[0] is not None
     assert results[0]['tax_subtotal'] == round(tier_amount_remaining * (tier.tier_rate / 100), 2)
 
+@pytest.mark.django_db
 def test_secondary_tier_calculate_where_primary_income_on_upper_boundary_and_total_exceeds():
     primary_income = 45000
     secondary_income = 20000
     tier = RuleTier(min_value=10000, max_value=45000, tier_rate=10)
     secondary_tier = SecondaryRuleTier(primary_tier=tier, tier_rate=10)
-    results = create_empty_results_table()
+    results = create_mock_test_calculation_result()
     secondary_tier.calculate(secondary_income, primary_income, results)
 
     tier_amount = tier.max_value - tier.min_value
@@ -160,12 +190,13 @@ def test_secondary_tier_calculate_where_primary_income_on_upper_boundary_and_tot
     assert results[0] is not None
     assert results[0]['tax_subtotal'] == round(tier_amount_remaining * (tier.tier_rate / 100), 2)
 
+@pytest.mark.django_db
 def test_secondary_tier_calculate_where_primary_income_above_upper_boundary():
     primary_income = 45001
     secondary_income = 20000
     tier = RuleTier(min_value=10000, max_value=45000, tier_rate=10)
     secondary_tier = SecondaryRuleTier(primary_tier=tier, tier_rate=10)
-    results = create_empty_results_table()
+    results = create_mock_test_calculation_result()
     secondary_tier.calculate(secondary_income, primary_income, results)
 
     tier_amount = tier.max_value - tier.min_value
@@ -173,11 +204,13 @@ def test_secondary_tier_calculate_where_primary_income_above_upper_boundary():
     assert len(results) == 0
 
 # Test iteration over rule tiers
+@pytest.mark.django_db
 def test_tiered_rule_iteration_with_no_tiers_defined():
     rule = TieredRateRule()
     rule.reset()
     assert rule.next() is None
 
+@pytest.mark.django_db
 def test_tiered_rule_iteration_with_single_tier_defined():
     tier = RuleTier(min_value=0, max_value=100)
     rule = TieredRateRule(first_tier=tier)
@@ -190,6 +223,7 @@ def test_tiered_rule_iteration_with_single_tier_defined():
     next_tier = rule.next()
     assert next_tier is None
 
+@pytest.mark.django_db
 def test_tiered_rule_iteration_with_multiple_tiers_defined():
     tier2 = RuleTier(min_value=101, max_value=200)
     tier1 = RuleTier(min_value=0, max_value=100, next=tier2)
@@ -208,11 +242,13 @@ def test_tiered_rule_iteration_with_multiple_tiers_defined():
     assert next_tier is None
 
 # Test iteration over secondary rule tiers
+@pytest.mark.django_db
 def test_secondary_tiered_rule_iteration_with_no_tiers_defined():
     rule = SecondaryTieredRateRule()
     rule.reset()
     assert rule.next() is None
 
+@pytest.mark.django_db
 def test_secondary_tiered_rule_iteration_with_single_tier_defined():
     ptier1 = RuleTier(min_value=0, max_value=100)
 
@@ -227,6 +263,7 @@ def test_secondary_tiered_rule_iteration_with_single_tier_defined():
     next_tier = rule.next()
     assert next_tier is None
 
+@pytest.mark.django_db
 def test_secondary_tiered_rule_iteration_with_multiple_tiers_defined():
     ptier2 = RuleTier(min_value=101, max_value=200)
     ptier1 = RuleTier(min_value=0, max_value=100, next=ptier2)
@@ -247,13 +284,15 @@ def test_secondary_tiered_rule_iteration_with_multiple_tiers_defined():
     next_tier = rule.next()
     assert next_tier is None
 
-# Test iteration over rules within a ruleset
+# Test iteration over rules within a ruleset/
+@pytest.mark.django_db
 def test_ruleset_iteration_with_no_rules_defined():
     rs = RuleSet()
     assert rs.next() is None
     rs.reset()
     assert rs.next() is None
 
+@pytest.mark.django_db
 def test_ruleset_iteration_with_single_rule_defined():
     rule1 = FlatRateRule(name='A')
     rs = RuleSet(first_rule=rule1)
@@ -263,6 +302,7 @@ def test_ruleset_iteration_with_single_rule_defined():
     assert next_rule.name == 'A'
     assert rs.next() is None
 
+@pytest.mark.django_db
 def test_ruleset_iteration_with_multiple_rules_defined():
     rule2 = FlatRateRule(name='B')
     rule1 = FlatRateRule(name='A', next=rule2)
