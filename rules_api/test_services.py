d2075d9de32a7fa4d8b79534e1f7cf6800304e93
James Naish <james.naish@ground-control.co.uk>
2023-12-13 20:14:26 +0000

    Define tests for rules API CRUD services

---
+++ rules_api/test_services.py
@@ -0,0 +1,552 @@
+from .models import *
+from .services import *
+from .views import *
+import pytest
+
+# Ruleset creation
+@pytest.mark.django_db
+def test_create_ruleset_with_null_data():
+    pass
+
+@pytest.mark.django_db
+def test_create_ruleset_with_null_jurisdiction_id():
+    pass
+
+@pytest.mark.django_db
+def test_create_ruleset_with_null_tax_category_id():
+    pass
+
+@pytest.mark.django_db
+def test_create_ruleset_with_non_existent_tax_category_id():
+    pass
+
+@pytest.mark.django_db
+def test_create_ruleset_with_duplicate_tax_category_jurisdiction_combination():
+    pass
+
+@pytest.mark.django_db
+def test_create_valid_ruleset():
+    pass
+
+# Ruleset deletion
+@pytest.mark.django_db
+def test_delete_ruleset_with_null_id():
+    pass
+
+@pytest.mark.django_db
+def test_delete_ruleset_with_non_existent_id():
+    pass
+
+@pytest.mark.django_db
+def test_delete_ruleset():
+    pass
+
+# Tax category creation
+@pytest.mark.django_db
+def test_create_tax_category_with_null_name():
+    pass
+
+@pytest.mark.django_db
+def test_create_tax_category_with_duplicate_name():
+    pass
+
+@pytest.mark.django_db
+def test_create_valid_tax_category():
+    pass
+
+#  Tax category deletion
+@pytest.mark.django_db
+def test_delete_tax_category_with_null_id():
+    pass
+
+@pytest.mark.django_db
+def test_delete_tax_category_with_non_existent_id():
+    pass
+
+@pytest.mark.django_db
+def test_delete_tax_category():
+    pass
+
+# Rule deletion
+@pytest.mark.django_db
+def test_delete_rule_with_null_id():
+    pass
+
+@pytest.mark.django_db
+def test_delete_rule_with_non_existent_id():
+    pass
+
+@pytest.mark.django_db
+def test_delete_rule():
+    pass
+
+# Flat rate rule creation
+@pytest.mark.django_db
+def test_create_flat_rate_rule_with_null_data():
+    pass
+
+@pytest.mark.django_db
+def test_create_flat_rate_rule_with_null_ruleset_id():
+    pass
+
+@pytest.mark.django_db
+def test_create_flat_rate_rule_with_non_existent_ruleset_id():
+    pass
+
+@pytest.mark.django_db
+def test_create_flat_rate_rule_with_non_numeric_ruleset_id():
+    pass
+
+@pytest.mark.django_db
+def test_crete_flat_rate_rule_with_null_name():
+    pass
+
+@pytest.mark.django_db
+def test_create_flat_rate_rule_with_null_ordinal():
+    pass
+
+@pytest.mark.django_db
+def test_create_flat_rate_rule_with_non_numeric_ordinal():
+    pass
+
+@pytest.mark.django_db
+def test_create_flat_rate_rule_with_nulL_explainer():
+    pass
+
+@pytest.mark.django_db
+def test_create_flat_rate_rule_with_null_variable_name():
+    pass
+
+@pytest.mark.django_db
+def test_create_flat_rate_rule_with_null_flat_rate():
+    pass
+
+@pytest.mark.django_db
+def test_create_flat_rate_rule_with_non_numeric_flat_rate():
+    pass
+
+@pytest.mark.django_db
+def test_create_flat_rate_rule_with_negative_flat_rate():
+    pass
+
+@pytest.mark.django_db
+def test_create_valid_flat_rate_rule():
+    pass
+
+# Flat rate rule updates
+@pytest.mark.django_db
+def test_update_flat_rate_rule_with_null_data():
+    pass
+
+@pytest.mark.django_db
+def test_update_flat_rate_rule_with_null_rule_id():
+    pass
+
+@pytest.mark.django_db
+def test_update_flat_rate_rule_with_non_existent_rule_id():
+    pass
+
+@pytest.mark.django_db
+def test_update_flat_rate_rule_with_non_numeric_rule_id():
+    pass
+
+@pytest.mark.django_db
+def test_update_flat_rate_rule_with_null_name():
+    pass
+
+@pytest.mark.django_db
+def test_update_flat_rate_rule_with_null_ordinal():
+    pass
+
+@pytest.mark.django_db
+def test_update_flat_rate_rule_with_non_numeric_ordinal():
+    pass
+
+@pytest.mark.django_db
+def test_update_flat_rate_rule_with_nulL_explainer():
+    pass
+
+@pytest.mark.django_db
+def test_update_flat_rate_rule_with_null_variable_name():
+    pass
+
+@pytest.mark.django_db
+def test_update_flat_rate_rule_with_null_flat_rate():
+    pass
+
+@pytest.mark.django_db
+def test_update_flat_rate_rule_with_non_numeric_flat_rate():
+    pass
+
+@pytest.mark.django_db
+def test_update_flat_rate_rule_with_negative_flat_rate():
+    pass
+
+@pytest.mark.django_db
+def test_update_valid_flat_rate_rule():
+    pass
+
+
+# Tiered rate rule creation
+@pytest.mark.django_db
+def test_create_tiered_rate_rule_with_null_data():
+    pass
+
+@pytest.mark.django_db
+def test_create_tiered_rate_rule_with_null_ruleset_id():
+    pass
+
+@pytest.mark.django_db
+def test_create_tiered_rate_rule_with_non_existent_ruleset_id():
+    pass
+
+@pytest.mark.django_db
+def test_create_tiered_rate_rule_with_non_numeric_ruleset_id():
+    pass
+
+@pytest.mark.django_db
+def test_crete_tiered_rate_rule_with_null_name():
+    pass
+
+@pytest.mark.django_db
+def test_create_tiered_rate_rule_with_null_ordinal():
+    pass
+
+@pytest.mark.django_db
+def test_create_tiered_rate_rule_with_non_numeric_ordinal():
+    pass
+
+@pytest.mark.django_db
+def test_create_tiered_rate_rule_with_nulL_explainer():
+    pass
+
+@pytest.mark.django_db
+def test_create_tiered_rate_rule_with_null_variable_name():
+    pass
+
+@pytest.mark.django_db
+def test_create_valid_tiered_rate_rule():
+    pass
+
+# Tiered rate rule updates
+@pytest.mark.django_db
+def test_update_tiered_rate_rule_with_null_data():
+    pass
+
+@pytest.mark.django_db
+def test_update_tiered_rate_rule_with_null_rule_id():
+    pass
+
+@pytest.mark.django_db
+def test_update_tiered_rate_rule_with_non_existent_rule_id():
+    pass
+
+@pytest.mark.django_db
+def test_update_tiered_rate_rule_with_non_numeric_rule_id():
+    pass
+
+@pytest.mark.django_db
+def test_update_tiered_rate_rule_with_null_name():
+    pass
+
+@pytest.mark.django_db
+def test_update_tiered_rate_rule_with_null_ordinal():
+    pass
+
+@pytest.mark.django_db
+def test_update_tiered_rate_rule_with_non_numeric_ordinal():
+    pass
+
+@pytest.mark.django_db
+def test_update_tiered_rate_rule_with_nulL_explainer():
+    pass
+
+@pytest.mark.django_db
+def test_update_tiered_rate_rule_with_null_variable_name():
+    pass
+
+@pytest.mark.django_db
+def test_update_valid_tiered_rate_rule():
+    pass
+
+# Rule tier creation
+@pytest.mark.django_db
+def test_create_rule_tier_with_null_data():
+    pass
+
+@pytest.mark.django_db
+def test_create_rule_tier_with_null_rule_id():
+    pass
+
+@pytest.mark.django_db
+def test_create_rule_tier_with_non_existent_rule_id():
+    pass
+
+@pytest.mark.django_db
+def test_create_rule_tier_with_non_numeric_rule_id():
+    pass
+
+@pytest.mark.django_db
+def test_create_rule_tier_with_null_min_value():
+    pass
+
+@pytest.mark.django_db
+def test_create_rule_tier_with_non_numeric_min_value():
+    pass
+
+@pytest.mark.django_db
+def test_create_rule_tier_with_null_max_value():
+    pass
+
+@pytest.mark.django_db
+def test_create_rule_tier_with_non_numeric_max_value():
+    pass
+
+@pytest.mark.django_db
+def test_create_rule_tier_with_null_ordinal():
+    pass
+
+@pytest.mark.django_db
+def test_create_rule_tier_with_non_numeric_ordinal():
+    pass
+
+@pytest.mark.django_db
+def test_create_rule_tier_with_null_tier_rate():
+    pass
+
+@pytest.mark.django_db
+def test_create_rule_tier_with_non_numeric_tier_rate():
+    pass
+
+@pytest.mark.django_db
+def test_create_valid_rule_tier():
+    pass
+
+# Rule tier updates
+@pytest.mark.django_db
+def test_update_rule_tier_with_null_data():
+    pass
+
+@pytest.mark.django_db
+def test_update_rule_tier_with_null_tier_id():
+    pass
+
+@pytest.mark.django_db
+def test_update_rule_tier_with_non_existent_tier_id():
+    pass
+
+@pytest.mark.django_db
+def test_update_rule_tier_with_non_numeric_tier_id():
+    pass
+
+@pytest.mark.django_db
+def test_update_rule_tier_with_null_min_value():
+    pass
+
+@pytest.mark.django_db
+def test_update_rule_tier_with_non_numeric_min_value():
+    pass
+
+@pytest.mark.django_db
+def test_update_rule_tier_with_null_max_value():
+    pass
+
+@pytest.mark.django_db
+def test_update_rule_tier_with_non_numeric_max_value():
+    pass
+
+@pytest.mark.django_db
+def test_update_rule_tier_with_null_ordinal():
+    pass
+
+@pytest.mark.django_db
+def test_update_rule_tier_with_non_numeric_ordinal():
+    pass
+
+@pytest.mark.django_db
+def test_update_rule_tier_with_null_tier_rate():
+    pass
+
+@pytest.mark.django_db
+def test_update_rule_tier_with_non_numeric_tier_rate():
+    pass
+
+@pytest.mark.django_db
+def test_update_valid_rule_tier():
+    pass
+
+# Secondary tiered rate rule creation
+@pytest.mark.django_db
+def test_create_secondary_tiered_rate_rule_with_null_data():
+    pass
+
+@pytest.mark.django_db
+def test_create_secondary_tiered_rate_rule_with_null_ruleset_id():
+    pass
+
+@pytest.mark.django_db
+def test_create_secondary_tiered_rate_rule_with_non_existent_ruleset_id():
+    pass
+
+@pytest.mark.django_db
+def test_create_secondary_tiered_rate_rule_with_non_numeric_ruleset_id():
+    pass
+
+@pytest.mark.django_db
+def test_create_secondary_tiered_rate_rule_with_null_primary_rule_id():
+    pass
+
+@pytest.mark.django_db
+def test_create_secondary_tiered_rate_rule_with_non_existent_primary_rule_id():
+    pass
+
+@pytest.mark.django_db
+def test_create_secondary_tiered_rate_rule_with_non_numeric_primary_rule_id():
+    pass
+
+@pytest.mark.django_db
+def test_crete_secondary_iered_rate_rule_with_null_name():
+    pass
+
+@pytest.mark.django_db
+def test_create_secondary_tiered_rate_rule_with_null_ordinal():
+    pass
+
+@pytest.mark.django_db
+def test_create_secondary_tiered_rate_rule_with_non_numeric_ordinal():
+    pass
+
+@pytest.mark.django_db
+def test_create_secondary_tiered_rate_rule_with_nulL_explainer():
+    pass
+
+@pytest.mark.django_db
+def test_create_secondary_tiered_rate_rule_with_null_variable_name():
+    pass
+
+@pytest.mark.django_db
+def test_create_valid_secondary_tiered_rate_rule():
+    pass
+
+# Secondary tiered rate rule updates
+@pytest.mark.django_db
+def test_update_secondary_tiered_rate_rule_with_null_data():
+    pass
+
+@pytest.mark.django_db
+def test_update_secondary_tiered_rate_rule_with_null_rule_id():
+    pass
+
+@pytest.mark.django_db
+def test_update_secondary_tiered_rate_rule_with_non_existent_rule_id():
+    pass
+
+@pytest.mark.django_db
+def test_update_secondary_tiered_rate_rule_with_non_numeric_rule_id():
+    pass
+
+@pytest.mark.django_db
+def test_update_secondary_iered_rate_rule_with_null_name():
+    pass
+
+@pytest.mark.django_db
+def test_update_secondary_tiered_rate_rule_with_null_ordinal():
+    pass
+
+@pytest.mark.django_db
+def test_update_secondary_tiered_rate_rule_with_non_numeric_ordinal():
+    pass
+
+@pytest.mark.django_db
+def test_update_secondary_tiered_rate_rule_with_nulL_explainer():
+    pass
+
+@pytest.mark.django_db
+def test_update_secondary_tiered_rate_rule_with_null_variable_name():
+    pass
+
+@pytest.mark.django_db
+def test_update_valid_secondary_tiered_rate_rule():
+    pass
+
+# Secondary rule tier creation
+@pytest.mark.django_db
+def test_create_secondary_rule_tier_with_null_data():
+    pass
+
+@pytest.mark.django_db
+def test_create_secondary_rule_tier_with_null_rule_id():
+    pass
+
+@pytest.mark.django_db
+def test_create_secondary_rule_tier_with_non_existent_rule_id():
+    pass
+
+@pytest.mark.django_db
+def test_create_secondary_rule_tier_with_non_numeric_rule_id():
+    pass
+
+@pytest.mark.django_db
+def test_create_secondary_rule_tier_with_null_primary_tier_id():
+    pass
+
+@pytest.mark.django_db
+def test_create_secondary_rule_tier_with_non_existent_primary_tier_id():
+    pass
+
+@pytest.mark.django_db
+def test_create_secondary_rule_tier_with_non_numeric_primary_tier_id():
+    pass
+
+@pytest.mark.django_db
+def test_create_secondary_rule_tier_with_null_tier_rate():
+    pass
+
+@pytest.mark.django_db
+def test_create_secondary_rule_tier_with_non_numeric_tier_rate():
+    pass
+
+@pytest.mark.django_db
+def test_create_valid_secondary_rule_tier():
+    pass
+
+# Secondary rule tier updates
+@pytest.mark.django_db
+def test_update_secondary_rule_tier_with_null_data():
+    pass
+
+@pytest.mark.django_db
+def test_update_secondary_rule_tier_with_null_tier_id():
+    pass
+
+@pytest.mark.django_db
+def test_update_secondary_rule_tier_with_non_existent_tier_id():
+    pass
+
+@pytest.mark.django_db
+def test_update_secondary_rule_tier_with_non_numeric_tier_id():
+    pass
+
+@pytest.mark.django_db
+def test_update_secondary_rule_tier_with_null_primary_tier_id():
+    pass
+
+@pytest.mark.django_db
+def test_update_secondary_rule_tier_with_non_existent_primary_tier_id():
+    pass
+
+@pytest.mark.django_db
+def test_update_secondary_rule_tier_with_non_numeric_primary_tier_id():
+    pass
+
+@pytest.mark.django_db
+def test_update_secondary_rule_tier_with_null_tier_rate():
+    pass
+
+@pytest.mark.django_db
+def test_update_secondary_rule_tier_with_non_numeric_tier_rate():
+    pass
+
+@pytest.mark.django_db
+def test_update_valid_secondary_rule_tier():
+    pass
