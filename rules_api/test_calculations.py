33475362d4ac6ae8af2f3cd495f9d2ef6dd28acf
James Naish <james.naish@ground-control.co.uk>
2023-12-14 09:46:11 +0000

    Implement get calculations tests

--- rules_api/test_calculations.py
+++ rules_api/test_calculations.py
@@ -1,18 +1,36 @@
 from .models import *
+from .test_models import *
+from .services import *
 import pytest
 
 # Retrieving calculations
 @pytest.mark.django_db
 def test_get_calculations_for_null_username():
-    pass
+    with pytest.raises(TaxCalculationResult.DoesNotExist):
+        get_calculations_for_user(None)
 
 @pytest.mark.django_db
 def test_get_calculations_for_non_existent_username():
-    pass
+    with pytest.raises(TaxCalculationResult.DoesNotExist):
+        get_calculations_for_user('Jimbo')
 
 @pytest.mark.django_db
 def test_get_calculations_for_valid_username():
-    pass
+    rule = create_mock_flat_rate_Rule('salary', 20, create_mock_ruleset())
+    assert rule is not None
+
+    jurisdiction_id = rule.ruleset.jurisdiction_id
+    calculation = create_calculation('bob', [jurisdiction_id], create_mock_variable_table())
+    assert calculation is not None
+    assert calculation.results.count() == 1
+    assert calculation.results.results.count() == 1
+
+    calculations = get_calculations_for_user('bob')
+    assert calculations.count() == 1
+    calculation = calculations.first()
+    assert calculation is not None
+    assert calculation.results.count() == 1
+    assert calculation.results.results.count() == 1
 
 # Generating calculations
 @pytest.mark.django_db
