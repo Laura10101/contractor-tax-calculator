d07e9c58ac1330c0093c965c86c00caf421a6f4c
James Naish <james.naish@ground-control.co.uk>
2023-12-14 18:45:57 +0000

    Make rules a subdomain of rulesets

--- rules_api/urls.py
+++ rules_api/urls.py
@@ -2,14 +2,14 @@ from django.urls import path, re_path
 from .views import *
 
 urlpatterns = [
-    path('', RuleList.as_view()),
-    path('<int:pk>/', RuleDetail.as_view()),
-    path('<int:rule_pk>/tiers/', RuleTiersList.as_view()),
-    path('<int:rule_pk>/tiers/<int:pk>/', RuleTierDetail.as_view()),
-    path('<int:rule_pk>/secondarytiers/', SecondaryRuleTiersList.as_view()),
-    path('<int:rule_pk>/secondarytiers/<int:pk>/', SecondaryRuleTierDetail.as_view()),
+    path('rulesets/<int:ruleset_pk>/rules/<int:rule_pk>/tiers/', RuleTiersList.as_view()),
+    path('rulesets/<int:ruleset_pk>/rules/<int:rule_pk>/tiers/<int:pk>/', RuleTierDetail.as_view()),
+    path('rulesets/<int:ruleset_pk>/rules/<int:rule_pk>/secondarytiers/', SecondaryRuleTiersList.as_view()),
+    path('rulesets/<int:ruleset_pk>/rules/<int:rule_pk>/secondarytiers/<int:pk>/', SecondaryRuleTierDetail.as_view()),
     path('rulesets/', RuleSetsList.as_view()),
     path('rulesets/<int:pk>/', RuleSetDetail.as_view()),
+    path('rulesets/<int:ruleset_pk>/rules/', RuleList.as_view()),
+    path('rulesets/<int:ruleset_pk>/rules/<int:pk>/', RuleDetail.as_view()),
     path('taxcategories/', TaxCategoriesList.as_view()),
     path('taxcategories/<int:pk>/', TaxCategoryDetail.as_view()),
 ]
