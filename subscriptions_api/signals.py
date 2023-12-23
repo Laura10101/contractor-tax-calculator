e8981094ebb3c6d431a98f8e40905fa3c152ed3a
Laura10101 <laurajanefindley@gmail.com>
2023-12-23 09:57:01 +0000

    Create non-active subscription when user is first created

--- subscriptions_api/signals.py
+++ subscriptions_api/signals.py
@@ -1,8 +1,13 @@
 from django.db.models.signals import post_save
+from django.contrib.auth.models import User
 
 from payments_api.models import Payment
 from .models import Subscription
-from .services import update_subscription
+from .services import create_subscription, update_subscription
+
+def trigger_subscription_creation(sender, instance, created, **kwargs):
+    if created:
+        create_subscription(instance.id, None)
 
 def trigger_subscription_update(sender, instance, created, **kwargs):
     if not created:
@@ -12,4 +17,5 @@ def trigger_subscription_update(sender, instance, created, **kwargs):
             subscription = Subscription.objects.get(pk=instance.subscription_id)
             update_subscription(subscription.user_id, instance.subscription_option_id)
 
+post_save.connect(trigger_subscription_creation, sender=User)
 post_save.connect(trigger_subscription_update, sender=Payment)
