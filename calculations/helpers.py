ce620b4a7597673fe491d382cd50a1bc9f2c3bea
Laura10101 <laurajanefindley@gmail.com>
2023-12-30 19:08:59 +0000

    Implement create_calculation helper in calculations app

--- calculations/helpers.py
+++ calculations/helpers.py
@@ -29,8 +29,40 @@ def get_forms_by_jurisdiction_ids(url, jurisdiction_ids):
         deserialised_data[int(jurisdiction_id_str)] = data['forms'][jurisdiction_id_str]
     return deserialised_data
 
-def create_calculation(url, data):
-    return 1
+def create_calculation(url, request):
+    body = {
+        'username': request.user.username,
+        'variables': {}
+    }
+    for key in request.POST:
+        print(key)
+        if not key == 'csrfmiddlewaretoken':
+            if key == 'jurisdiction_ids':
+                body['jurisdiction_ids'] = [int(id) for id in request.POST[key].split(',')]
+            else:
+                body['variables'][key] = cast(request.POST[key])
+
+    print(body)
+    response = requests.post(url, json=body)
+    if response.ok:
+        print(json.loads(response.text))
+        return json.loads(response.text)
+    else:
+        print(json.loads(response.text))
+        raise Exception('Posting calculation failed with status code=' + str(response.status_code) + ' and error = ' + json.loads(response.text)['error'])
+    
 
 def get_calculation(url, id):
     return {}
+
+def cast(value):
+    if value.lower() in ['true', 'false']:
+        return value.lower() == 'true'
+    else:
+        try:
+            return int(value)
+        except ValueError:
+            try:
+                return float(value)
+            except ValueError:
+                return value
