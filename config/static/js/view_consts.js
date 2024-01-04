b14bba27a63986830ea144f726b6c5ac462d44f8
Laura10101 <laurajanefindley@gmail.com>
2024-01-04 19:32:39 +0000

    Define question dialog constants

---
+++ config/static/js/view_consts.js
@@ -0,0 +1,216 @@
+/*
+ * view_consts.js
+ * Defines constants used to simplify access to key DOM elements and other resources
+ */
+
+/*
+ * Question Dialog Constants
+ */
+questionTypeDialog = {
+    dialog: {
+        id: ""
+    },
+
+    label: {
+        id: ""
+    },
+
+    questionType: {
+        label: {
+            id: ""
+        },
+
+        input: {
+            id: ""
+        }
+    }
+};
+
+booleanQuestionDialog = {
+    dialog: {
+        id: ""
+    },
+
+    label: {
+        id: ""
+    },
+
+    questionText: {
+        label: {
+            id: ""
+        },
+        input: {
+            id: ""
+        }
+    },
+
+    explainer: {
+        label: {
+            id: ""
+        },
+        input: {
+            id: ""
+        }
+    },
+
+    variableName: {
+        label: {
+            id: ""
+        },
+        input: {
+            id: ""
+        }
+    },
+
+    isMandatory: {
+        label: {
+            id: ""
+        },
+        input: {
+            id: ""
+        }
+    }
+};
+
+numericQuestionDialog = {
+    dialog: {
+        id: ""
+    },
+
+    label: {
+        id: ""
+    },
+
+    questionText: {
+        label: {
+            id: ""
+        },
+        input: {
+            id: ""
+        }
+    },
+
+    explainer: {
+        label: {
+            id: ""
+        },
+        input: {
+            id: ""
+        }
+    },
+
+    variableName: {
+        label: {
+            id: ""
+        },
+        input: {
+            id: ""
+        }
+    },
+
+    isMandatory: {
+        label: {
+            id: ""
+        },
+        input: {
+            id: ""
+        }
+    },
+
+    isInteger: {
+        label: {
+            id: ""
+        },
+        input: {
+            id: ""
+        }
+    },
+
+    minimumValue: {
+        label: {
+            id: ""
+        },
+        input: {
+            id: ""
+        }
+    },
+
+    maxiumValue: {
+        label: {
+            id: ""
+        },
+        input: {
+            id: ""
+        }
+    }
+};
+
+multipleChoiceQuestionDialog = {
+    dialog: {
+        id: ""
+    },
+
+    label: {
+        id: ""
+    },
+
+    questionText: {
+        label: {
+            id: ""
+        },
+        input: {
+            id: ""
+        }
+    },
+
+    explainer: {
+        label: {
+            id: ""
+        },
+        input: {
+            id: ""
+        }
+    },
+
+    variableName: {
+        label: {
+            id: ""
+        },
+        input: {
+            id: ""
+        }
+    },
+
+    isMandatory: {
+        label: {
+            id: ""
+        },
+        input: {
+            id: ""
+        }
+    },
+
+    allowMultiselect: {
+        label: {
+            id: ""
+        },
+        input: {
+            id: ""
+        }
+    },
+
+    options: {
+        label: {
+            id: ""
+        },
+        table: {
+            id: ""
+        },
+        optionRow: {
+            id: "",
+            textCell: {
+                id: ""
+            }
+        }
+    }
+};
