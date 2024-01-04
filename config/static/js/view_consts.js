8b42571f8d445e9dc9d0cfd35712c06e849923a8
Laura10101 <laurajanefindley@gmail.com>
2024-01-04 19:54:04 +0000

    Define constants for rule dialogs in config app

--- config/static/js/view_consts.js
+++ config/static/js/view_consts.js
@@ -214,3 +214,290 @@ multipleChoiceQuestionDialog = {
         }
     }
 };
+
+/*
+ * Question Display Constants
+ */
+
+/*
+ * Rule Dialog Constants
+ */
+rulesetDialog = {
+    dialog: {
+        id: ""
+    },
+
+    label: {
+        id: ""
+    },
+
+    taxCategory: {
+        label: {
+            id: ""
+        },
+
+        input: {
+            id: ""
+        }
+    }
+};
+
+ruleTypeDialog = {
+    dialog: {
+        id: ""
+    },
+
+    label: {
+        id: ""
+    },
+
+    ruleType: {
+        label: {
+            id: ""
+        },
+
+        input: {
+            id: ""
+        }
+    }
+};
+
+flatRateRuleDialog = {
+    dialog: {
+        id: ""
+    },
+
+    label: {
+        id: ""
+    },
+
+    name: {
+        label: {
+            id: ""
+        },
+        input: {
+            id: ""
+        }
+    },
+
+    explainer: {
+        label: {
+            id: ""
+        },
+        input: {
+            id: ""
+        }
+    },
+
+    variableName: {
+        label: {
+            id: ""
+        },
+        input: {
+            id: ""
+        }
+    },
+
+    taxRate: {
+        label: {
+            id: ""
+        },
+        input: {
+            id: ""
+        }
+    }
+};
+
+tieredRateRuleDialog = {
+    dialog: {
+        id: ""
+    },
+
+    label: {
+        id: ""
+    },
+
+    name: {
+        label: {
+            id: ""
+        },
+        input: {
+            id: ""
+        }
+    },
+
+    explainer: {
+        label: {
+            id: ""
+        },
+        input: {
+            id: ""
+        }
+    },
+
+    variableName: {
+        label: {
+            id: ""
+        },
+        input: {
+            id: ""
+        }
+    },
+
+    tiers: {
+        label: {
+            id: ""
+        },
+        table: {
+            id: ""
+        },
+        tierRow: {
+            id: "",
+            lowerLimit: {
+                cellId: ""
+            },
+            upperLimit: {
+                cellId: ""
+            },
+            taxRate: {
+                cellId: ""
+            }
+        }
+    }
+};
+
+secondaryTieredRateRuleDialog = {
+    dialog: {
+        id: ""
+    },
+
+    label: {
+        id: ""
+    },
+
+    name: {
+        label: {
+            id: ""
+        },
+        input: {
+            id: ""
+        }
+    },
+
+    explainer: {
+        label: {
+            id: ""
+        },
+        input: {
+            id: ""
+        }
+    },
+
+    variableName: {
+        label: {
+            id: ""
+        },
+        input: {
+            id: ""
+        }
+    },
+
+    primaryRule: {
+        label: {
+            id: ""
+        },
+        input: {
+            id: ""
+        }
+    },
+
+    tiers: {
+        label: {
+            id: ""
+        },
+        table: {
+            id: ""
+        },
+        tierRow: {
+            id: "",
+            lowerLimit: {
+                cellId: ""
+            },
+            upperLimit: {
+                cellId: ""
+            },
+            taxRate: {
+                cellId: ""
+            }
+        }
+    }
+};
+
+ruleTierDialog = {
+    dialog: {
+        id: ""
+    },
+
+    label: {
+        id: ""
+    },
+
+    minimumValue: {
+        label: {
+            id: ""
+        },
+        input: {
+            id: ""
+        }
+    },
+
+    maximumValue: {
+        label: {
+            id: ""
+        },
+        input: {
+            id: ""
+        }
+    },
+
+    taxRate: {
+        label: {
+            id: ""
+        },
+        input: {
+            id: ""
+        }
+    }
+};
+
+secondaryRuleTierDialog = {
+    dialog: {
+        id: ""
+    },
+
+    label: {
+        id: ""
+    },
+
+    primaryTier: {
+        label: {
+            id: ""
+        },
+        input: {
+            id: ""
+        }
+    },
+
+    taxRate: {
+        label: {
+            id: ""
+        },
+        input: {
+            id: ""
+        }
+    }
+};
+
+/*
+ * Rule Display Constants
+ */
