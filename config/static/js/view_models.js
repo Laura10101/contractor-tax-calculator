cb1732631ea2209ae27607dee720758c2e40da33
Laura10101 <laurajanefindley@gmail.com>
2024-01-08 14:23:01 +0000

    Define view model for config app£

---
+++ config/static/js/view_models.js
@@ -0,0 +1,10 @@
+/*
+ * view_models.js
+ * Holds the current state of the config app, providing global access
+ * to state data 
+ */
+let app = {
+    jurisdictions: [],
+    jurisdictionRules: [],
+    jurisdictionQuestions: []
+}
