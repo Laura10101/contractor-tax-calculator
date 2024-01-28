f8550bc9f6f584e47a716f9d7eb3028ce5076848
Laura10101 <laurajanefindley@gmail.com>
2024-01-28 09:59:38 +0000

    Add link to config app in admin site

--- config/admin.py
+++ config/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.urls import path
+from .views import config
 
 # Register your models here.
+class ConfigAdmin(admin.ModelAdmin):
+    def get_urls(self):
+        urls = super().get_urls()
+        urls += [path("config/", self.admin_site.admin_view(config))]
+        return urls
--- config/views.py
+++ config/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.contrib import admin
 
 # Create your views here.
+@admin.site.admin_view
 def config(request):
     template = 'config/config.html'
     return render(request, template)
