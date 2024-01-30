acfa2b38f8337e515475b9c30772321a076ba6b6
Laura10101 <laurajanefindley@gmail.com>
2024-01-30 11:17:53 +0000

    Comment validate.js in the calculations app

--- calculations/static/js/validation.js
+++ calculations/static/js/validation.js
@@ -1,25 +1,33 @@
+
+// A booelan field is valid if exactly one response has been checked
 function isBooleanFieldValid(id) {
     return $('input:radio[name="' + id + '"]:checked').length == 1;
 }
 
+// A float field is valid only if a valid integer or floating point nunmber has been entered
 function isFloatFieldValid(id) {
     let value = document.getElementById(id).value;
     return !isNaN(parseFloat(value)) && isFinite(value);
 }
 
+
+// An integer is valid only if a valid integer has been entered
 function isIntegerFieldValid(id) {
     let value = document.getElementById(id).value;
     return !isNaN(parseInt(value)) && isFinite(value);
 }
 
+// A single select is valid if exactly one box has been checked
 function isSingleSelectFieldValid(id) {
     return $('input[name="' + id + '"]:checked').length == 1;
 }
 
+// Concatenate all errors and display in the error box
 function showFormErrors(errors) {
     document.getElementById("errors").innerHTML = errors.join("");
 }
 
+// Get the label corresponding to the given field
 function getLabelValue(id) {
     return document.getElementById(id + "-title").innerHTML;
 }
@@ -37,6 +45,8 @@ function deduplicate(arr) {
     return unique;
 }
 
+// Iterate through each field and validate it, adding the
+// errors if a field is not valid. Submit the form only if all fields are valid
 function validateForm() {
     let errors = [];
 
