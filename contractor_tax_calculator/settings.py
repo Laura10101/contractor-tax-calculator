7adbfdc243a9127eedd9791bb1a3382515fad93b
Laura10101 <laurajanefindley@gmail.com>
2024-01-30 23:05:58 +0000

    Remove static files app

--- contractor_tax_calculator/settings.py
+++ contractor_tax_calculator/settings.py
@@ -46,7 +46,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'forms_api',
     'jurisdictions_api',
     'rules_api',
