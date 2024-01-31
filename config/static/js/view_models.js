3cb9975c78cbe580c9a63751595fee38437fae2f
Laura10101 <laurajanefindley@gmail.com>
2024-01-31 09:58:52 +0000

    Add comments to view_models.js

--- config/static/js/view_models.js
+++ config/static/js/view_models.js
@@ -1,4 +1,9 @@
 /* jshint esversion: 8 */
+/*
+ * view_models.js
+ * Holds the current state of the config app, providing global access
+ * to state data 
+ */
 
 if (typeof require !== "undefined") {
     const viewConsts = require("./view_consts");
@@ -6,9 +11,10 @@ if (typeof require !== "undefined") {
 }
 
 /*
- * view_models.js
- * Holds the current state of the config app, providing global access
- * to state data 
+ * App State
+ * Holds the current state of the application including referential data (questions, rulesets, rules),
+ * the entity currently being edited, and any parent entity
+ * 
  */
 let app = {
     apiHost: {
@@ -43,6 +49,7 @@ function setApiHost(protocol, hostname) {
 /*
  * State Management View Model Methods
  */
+// Clear the entity currently being acted on (created, updated, deleted)
 function clearDialogState() {
     app.dialogState = {
         mode: null,
@@ -52,6 +59,7 @@ function clearDialogState() {
     app.parentRuleset = null
 }
 
+// Set the entity currently being acted on (created, updated, deleted)
 function setDialogState(mode, entityType, entity) {
     app.dialogState = {
         mode: mode,
@@ -60,6 +68,7 @@ function setDialogState(mode, entityType, entity) {
     };
 }
 
+// Set the parent of the entity currently being acted on (created, updated, deleted)
 function setParentState(mode, entityType, entity) {
     app.parentState = {
         mode: mode,
@@ -68,6 +77,7 @@ function setParentState(mode, entityType, entity) {
     };
 }
 
+// Clear the parent of the entity currently being acted on (created, updated, deleted)
 function clearParentState() {
     app.parentState = {
         mode: null,
@@ -76,22 +86,25 @@ function clearParentState() {
     };
 }
 
+// Move the entity currently being acted on into the parent state
 function moveAppStateToParentState() {
     app.parentState = app.dialogState;
     clearDialogState();
 }
 
+// Move the entity currently being acted on into the parent state
 function moveParentStateToAppState() {
     app.dialogState = app.parentState;
     clearParentState();
 }
 
+// Set the ruleset that is the parent of the rule currently being acted on
 function setParentRuleset(ruleset) {
     app.parentRuleset = ruleset;
 }
 
 // Refresh any entities that are held within the app state after
-// refreshing the 
+// refreshing the referential data
 function refreshAppState() {
     // Update parent ruleset
     if (app.parentRuleset != null) {
@@ -184,7 +197,7 @@ function refreshAppState() {
 }
 
 /*
- * View Model Accessor Methods
+ * View Model Accessor Methods - Forms
  */
 function getForm() {
     return app.jurisdictionForm.forms[Object.keys(app.jurisdictionForm.forms)[0]];
@@ -194,6 +207,10 @@ function getFormId() {
     return getForm().id;
 }
 
+
+/*
+ * View Model Accessor Methods - Tax Categories
+ */
 function getTaxCategoryById(id) {
     let category = null;
     app.taxCategories.forEach(taxCategory => {
@@ -204,14 +221,21 @@ function getTaxCategoryById(id) {
     return category;
 }
 
+/*
+ * View Model Accessor Methods - Questions
+ */
+
+// Extract questions from the form
 function getQuestions() {
     return app.jurisdictionForm.forms[Object.keys(app.jurisdictionForm.forms)[0]].questions;
 }
 
+// Calculate the next question ordinal in the current sequence
 function getNextQuestionOrdinal() {
     return getQuestions().length + 1;
 }
 
+// Get a question based on its database ID
 function findQuestionById(questionId) {
     let question = null;
 
@@ -224,6 +248,9 @@ function findQuestionById(questionId) {
     return question;
 }
 
+// Extract the list of variable names from current questions
+// For now, non-numeric questions are excluded as rules are not able
+// currently to support responses from non-numeric questions
 function getValidQuestionTextVariableNamePairs() {
     let questions = getQuestions();
     let variables = [];
@@ -244,6 +271,7 @@ function getValidQuestionTextVariableNamePairs() {
     });
 }
 
+// Check if the variable name provided is a duplicate of the existing variable names
 function isDuplicateVariableName(variableName) {
     let isDuplicate = false;
     let existingVariableNames = getValidQuestionTextVariableNamePairs();
@@ -255,6 +283,11 @@ function isDuplicateVariableName(variableName) {
     return isDuplicate;
 }
 
+/*
+ * View Model Accessor Methods - Multiple choice options
+ */
+
+// Find a multiple choice option based on its ID
 function findMultiplpeChoiceOptionById(optionId) {
     let option = null;
 
@@ -271,6 +304,9 @@ function findMultiplpeChoiceOptionById(optionId) {
     return option;
 }
 
+/*
+ * View Model Accessor Methods - Rulesets
+ */
 function findRulesetById(id) {
     let ruleset = null;
     app.jurisdictionRules.forEach(candidateRuleset => {
@@ -281,10 +317,11 @@ function findRulesetById(id) {
     return ruleset;
 }
 
-function getTieredRateRulesForJurisdiction() {
-    return getRulesByTypeForJurisdiction('tiered_rate');
-}
+/*
+ * View Model Accessor Methods - Rules
+ */
 
+// Get a rule based on its database ID
 function findRuleById(ruleId) {
     let rule = null;
 
@@ -299,6 +336,8 @@ function findRuleById(ruleId) {
     return rule;
 }
 
+// Check if there are any secondary tiered rules that have the specified
+// tiered rate rule as their primary rule
 function primaryRuleHasDependentSecondaryRules(primaryRuleId) {
     let hasDependents = false;
     let primaryRule = findRuleById(primaryRuleId);
@@ -314,6 +353,7 @@ function primaryRuleHasDependentSecondaryRules(primaryRuleId) {
     return hasDependents;
 }
 
+// Find the ruleset to which the specified rule belongs
 function findParentRuleset(ruleId) {
     let ruleset = null;
 
@@ -328,6 +368,29 @@ function findParentRuleset(ruleId) {
     return ruleset;
 }
 
+// Get all of the currently-loaded tiered rate rules
+function getTieredRateRulesForJurisdiction() {
+    return getRulesByTypeForJurisdiction('tiered_rate');
+}
+
+// Get all of the currently loaded rules for the specified type
+function getRulesByTypeForJurisdiction(type) {
+    rules = [];
+    app.jurisdictionRules.forEach(ruleset => {
+        ruleset.rules.forEach(rule => {
+            if (rule.type == type) {
+                rules.push(rule);
+            }
+        });
+    });
+    return rules;
+}
+
+/*
+ * View Model Accessor Methods - Rule Tiers
+ */
+
+// Return a rule tier based on its database ID
 function findPrimaryRuleTierById(tierId) {
     let tier = null;
 
@@ -346,6 +409,7 @@ function findPrimaryRuleTierById(tierId) {
     return tier;
 }
 
+// Return a secondary rule tier based on its database ID
 function findSecondaryRuleTierById(tierId) {
     let tier = null;
 
@@ -364,6 +428,8 @@ function findSecondaryRuleTierById(tierId) {
     return tier;
 }
 
+// Check if the any secondary rule tier has the specified rule tier
+// as its primary tier
 function primaryRuleTierHasDependentSecondaryTiers(primaryTierId) {
     let hasDependents = false;
     let primaryTier = findPrimaryRuleTierById(primaryTierId);
@@ -382,8 +448,11 @@ function primaryRuleTierHasDependentSecondaryTiers(primaryTierId) {
 }
 
 /*
- * Ordinal Traversal
+ * Ordinal Traversal - Questions
  */
+
+// Find the question that immediately precedes the specified one
+// based on ordinals
 function findPreviousQuestion(question) {
     if (question == null || question.ordinal == null) {
         return null;
@@ -407,6 +476,8 @@ function findPreviousQuestion(question) {
     return previousQuestion;
 }
 
+// Find the question that immediately follows the specified one
+// based on ordinals
 function findNextQuestion(question) {
     if (question == null || question.ordinal == null) {
         return null;
@@ -430,6 +501,11 @@ function findNextQuestion(question) {
     return nextQuestion;
 }
 
+// Deleting an entity leaves a gap in the ordinal sequence
+// E.g., deleting entity with ordinal = 2 will leave the remaining
+// ordinlas 1 and 3
+// This procedure resequences ordinals so that the entities
+// remain in the same order but with no gaps
 function resequenceQuestionOrdinals(deletedQuestion) {
     let questions = getQuestions();
     let i = 0;
@@ -442,10 +518,18 @@ function resequenceQuestionOrdinals(deletedQuestion) {
     return questions;
 }
 
+/*
+ * Ordinal Traversal - Rulesets
+ */
+
+// Find the next ruleset ordinal in the current sequence for the selected
+// jurisdiction
 function getNextRulesetOrdinal() {
     return app.jurisdictionRules.length + 1;
 }
 
+// Find the ruleset that immediately precedes the specified one
+// based on ordinals
 function findPreviousRuleset(ruleset) {
     if (ruleset == null || ruleset.ordinal == null) {
         return null;
@@ -469,6 +553,8 @@ function findPreviousRuleset(ruleset) {
     return previousRuleset;
 }
 
+// Find the ruleset that immediately precedes the specified one
+// based on ordinals
 function findNextRuleset(ruleset) {
     if (ruleset == null || ruleset.ordinal == null) {
         return null;
@@ -492,6 +578,11 @@ function findNextRuleset(ruleset) {
     return nextRuleset;
 }
 
+// Deleting an entity leaves a gap in the ordinal sequence
+// E.g., deleting entity with ordinal = 2 will leave the remaining
+// ordinlas 1 and 3
+// This procedure resequences ordinals so that the entities
+// remain in the same order but with no gaps
 function resequenceRulesetOrdinals(deletedRuleset) {
     let rulesets = app.jurisdictionRules;
     let i = 0;
@@ -504,18 +595,11 @@ function resequenceRulesetOrdinals(deletedRuleset) {
     return rulesets;
 }
 
-function getRulesByTypeForJurisdiction(type) {
-    rules = [];
-    app.jurisdictionRules.forEach(ruleset => {
-        ruleset.rules.forEach(rule => {
-            if (rule.type == type) {
-                rules.push(rule);
-            }
-        });
-    });
-    return rules;
-}
+/*
+ * Ordinal Traversal - Rules
+ */
 
+// Get the next ordinal in the sequence
 function getNextRuleOrdinal() {
     if (app.parentRuleset != null) {
         return app.parentRuleset.rules.length + 1;
@@ -524,6 +608,8 @@ function getNextRuleOrdinal() {
     }
 }
 
+// Find the rule that immediately precedes the specified one
+// based on ordinals
 function findPreviousRule(ruleset, rule) {
     if (ruleset == null || rule == null || ruleset.rules == null || rule.ordinal == null) {
         return null;
@@ -547,6 +633,8 @@ function findPreviousRule(ruleset, rule) {
     return previousRule;
 }
 
+// Find the question that immediately follows the specified one
+// based on ordinals
 function findNextRule(ruleset, rule) {
     if (ruleset == null || rule == null || ruleset.rules == null || rule.ordinal == null) {
         return null;
@@ -570,6 +658,11 @@ function findNextRule(ruleset, rule) {
     return nextRule;
 }
 
+// Deleting an entity leaves a gap in the ordinal sequence
+// E.g., deleting entity with ordinal = 2 will leave the remaining
+// ordinlas 1 and 3
+// This procedure resequences ordinals so that the entities
+// remain in the same order but with no gaps
 function resequenceRuleOrdinals(deletedRule) {
     let rules = app.parentRuleset.rules;
     let i = 0;
@@ -582,6 +675,11 @@ function resequenceRuleOrdinals(deletedRule) {
     return rules;
 }
 
+/*
+ * Ordinal Traversal - Rule Tiers
+ */
+
+// Find the next rule tier based on the current sequence
 function getNextRuleTierOrdinal(rule) {
     if (rule != null) {
         if (rule.type == "tiered_rate" || rule.type == "secondary_tiered_rate") {
@@ -594,6 +692,8 @@ function getNextRuleTierOrdinal(rule) {
     }
 }
 
+// Find the rule tier that immediately precedes the specified one
+// based on ordinals
 function findPreviousRuleTier(rule, tier) {
     if (rule == null || tier == null || rule.tiers == null || tier.ordinal == null) {
         return null;
@@ -616,6 +716,8 @@ function findPreviousRuleTier(rule, tier) {
     return previousTier;
 }
 
+// Find the rule tier that immediately precedes the specified one
+// based on ordinals
 function findNextRuleTier(rule, tier) {
     if (rule == null || tier == null || rule.tiers == null || tier.ordinal == null) {
         return null;
@@ -639,6 +741,11 @@ function findNextRuleTier(rule, tier) {
     return nextTier;
 }
 
+// Deleting an entity leaves a gap in the ordinal sequence
+// E.g., deleting entity with ordinal = 2 will leave the remaining
+// ordinlas 1 and 3
+// This procedure resequences ordinals so that the entities
+// remain in the same order but with no gaps
 function resequenceRuleTierOrdinals(deletedTier) {
     let tiers = app.parentState.entity.tiers;
     let i = 0;
@@ -651,6 +758,9 @@ function resequenceRuleTierOrdinals(deletedTier) {
     return tiers;
 }
 
+/*
+ * Exports
+ */
 if (typeof module !== "undefined") module.exports = {
     app,
     clearDialogState,
