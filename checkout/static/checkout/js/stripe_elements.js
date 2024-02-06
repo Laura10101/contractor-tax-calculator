cc1a177cf960c1a02b684bf2bb221cc11ce70865
Laura10101 <laurajanefindley@gmail.com>
2024-02-06 21:51:57 +0000

    Add additional comments to Stripe elements JS file

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,21 +1,26 @@
 /* jshint esversion: 8 */
+// Create Stripe and Stripe Elements instance
 var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
 var client_secret = $('#id_client_secret').text().slice(1, -1);
 var stripe = Stripe(stripe_public_key);
 var elements = stripe.elements({ clientSecret: client_secret });
 
+// Styling for card element
 var style = {
     base: {
       color: "#000000",
     }
   };
 
+// Create address and card elements
 const address = elements.create('address', { mode: 'billing' });
 const card = elements.create('card');
 
+// Mount card and address elements
 address.mount('#address-element');
 card.mount('#card-element', {style: style});
 
+// Check if address and card elements are valid
 function elementsAreValid()  {
   let addressELementDiv = document.getElementById("address-element");
   let cardELementDiv = document.getElementById("card-element");
@@ -36,7 +41,9 @@ address.addEventListener('change', function (event) {
 // Handle realtime validation errors on the card element
 card.addEventListener('change', function (event) {
   var errorDiv = document.getElementById('card-errors');
+  // If the event is an error
   if (event.error) {
+      // Display the error
       var html = `
           <span class="icon" role="alert">
               <i class="fas fa-times"></i>
@@ -45,6 +52,7 @@ card.addEventListener('change', function (event) {
       `;
       $(errorDiv).html(html);
   } else {
+      // Otherwise, submit the form
       let submit = document.getElementById("submit-button");
       errorDiv.textContent = '';
       if (elementsAreValid()) {
