81512a9fb01972a5cf86894b490aec1a447a8ec8
Laura10101 <laurajanefindley@gmail.com>
2024-02-07 08:01:50 +0000

    Fix linting issues in custom storages file

--- custom_storages.py
+++ custom_storages.py
@@ -1,7 +1,9 @@
-""" Defines the StaticStorage class providing access to AWS storage for static files """
+"""Defines the location for AWS static files storage."""
 from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
+
 class StaticStorage(S3Boto3Storage):
-    """ Provide access to the AWS storage location for static files """
+    """Provide access to the AWS storage location for static files."""
+
     location = settings.STATICFILES_LOCATION
