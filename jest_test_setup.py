47f05fd132ff14c52bd9abd46cf6c57d301dbfdc
Laura10101 <laurajanefindley@gmail.com>
2024-02-07 07:58:40 +0000

    Fix linting issues in jest test setup script

--- jest_test_setup.py
+++ jest_test_setup.py
@@ -1,6 +1,4 @@
-""" This script is used to set up the test database prior to running Jest tests """
-
-#!/usr/bin/env python
+"""Set up the test database prior to running Jest tests."""
 import os
 import django
 from django.core.management import execute_from_command_line
@@ -21,7 +19,10 @@ else:
 # Use Django in Standalone mode
 # https://docs.djangoproject.com/en/5.0/topics/settings/
 print("Specifying settings module")
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'contractor_tax_calculator.settings')
+os.environ.setdefault(
+    'DJANGO_SETTINGS_MODULE',
+    'contractor_tax_calculator.settings'
+    )
 
 print("Setting up Django in standalone mode")
 django.setup()
@@ -40,6 +41,6 @@ print("Setting up test data - tax categories")
 names = ['Income Tax', 'Dividend Tax', 'Corporation Tax', 'VAT']
 for name in names:
     tax_category_id = create_tax_category(name)
-    print('Created tax cateogry with name=' + name + ' and id=' + str(tax_category_id))
+    print('Created tax cateogry ' + name + ' and id=' + str(tax_category_id))
 
 print("Ready to go")
