a920215a1f4ce15a7b1054d1307bc412ad58d4e7
Laura10101 <laurajanefindley@gmail.com>
2024-02-07 08:49:31 +0000

    Fix linting issues in calculation URLs file

--- calculations/urls.py
+++ calculations/urls.py
@@ -1,8 +1,13 @@
+"""Define urls for the calculations app."""
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.display_calculation, name='display_calculation'),
-    path('jurisdictions/select', views.select_jurisdictions, name='select_jurisdictions'),
+    path(
+        'jurisdictions/select',
+        views.select_jurisdictions,
+        name='select_jurisdictions'
+    ),
     path('form', views.display_form, name='display_form'),
 ]
