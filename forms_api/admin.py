7b8ad8491599c4b1274ac15ffebb10caa5a5ddf0
Laura10101 <laurajanefindley@gmail.com>
2024-02-07 10:07:18 +0000

    Fix linting issues in forms API admin configuration

--- forms_api/admin.py
+++ forms_api/admin.py
@@ -1,9 +1,21 @@
+"""Add the form model to the admin app."""
+
 from django.contrib import admin
-from .models import Form, BooleanQuestion, MultipleChoiceQuestion, MultipleChoiceOption, NumericQuestion
+from .models import (
+    Form,
+    BooleanQuestion,
+    MultipleChoiceQuestion,
+    MultipleChoiceOption,
+    NumericQuestion
+)
 
 from .forms import FormForm
 
+
 class FormAdmin(admin.ModelAdmin):
+    """Admin configuration for the form model"""
+
     form = FormForm
+
 
 admin.site.register(Form, FormAdmin)
