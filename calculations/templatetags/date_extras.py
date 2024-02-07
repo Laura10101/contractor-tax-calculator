9ea2b0f556fd0d66f8ce694de00df9f10914b48f
Laura10101 <laurajanefindley@gmail.com>
2024-02-07 17:00:33 +0000

    Fix linting issues in date_extras

--- calculations/templatetags/date_extras.py
+++ calculations/templatetags/date_extras.py
@@ -1,14 +1,19 @@
+"""Define custom template tags for processing dates."""
 from django import template
 from datetime import datetime
 
+
 def datestring(value):
+    """Return a formatted date from a string."""
+
     if value is None:
         return ''
     try:
         input_datetime = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%fZ")
-    except:
+    except Exception:
         input_datetime = datetime.strptime(value, "%Y-%m-%dT%H:%M:%SZ")
     return input_datetime.strftime("%d-%m-%Y %H:%M:%S")
+
 
 register = template.Library()
 register.filter("datestring", datestring)
