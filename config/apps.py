83a36a0ef681810547e41e3ed1538c7124759470
Laura10101 <laurajanefindley@gmail.com>
2024-02-07 09:50:09 +0000

    Fix linting issues in config apps.py

--- config/apps.py
+++ config/apps.py
@@ -1,6 +1,9 @@
+"""Defines the config class for the config app."""
 from django.apps import AppConfig
 
 
 class ConfigConfig(AppConfig):
+    """Configuration for the config app."""
+
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'config'
