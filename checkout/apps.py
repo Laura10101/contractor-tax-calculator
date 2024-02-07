097cdc7db2edf44d77f399840904c32f26f94418
Laura10101 <laurajanefindley@gmail.com>
2024-02-07 09:21:24 +0000

    Fix linting issues in checkout apps config

--- checkout/apps.py
+++ checkout/apps.py
@@ -1,6 +1,9 @@
+"""Defines the config class for the checkout app."""
 from django.apps import AppConfig
 
 
 class CheckoutConfig(AppConfig):
+    """Configure the checkout app."""
+
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'checkout'
