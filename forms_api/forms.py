3ef7a8d4756ca9376882dc81665acae175fd1cde
Laura10101 <laurajanefindley@gmail.com>
2024-02-07 10:15:49 +0000

    Fix linting issues for Forms API forms.py

--- forms_api/forms.py
+++ forms_api/forms.py
@@ -1,13 +1,16 @@
+"""Define admin form for the Form model."""
 from django import forms
 from dal import autocomplete
 
 from .models import Form
 
 class FormForm(forms.ModelForm):
+    """Configuration for the Form model admin form."""
+
     class Meta:
         model = Form
         widgets = {
-            'jurisdiction_id': autocomplete.ListSelect2('jurisdiction-autocomplete')
+            'jurisdiction_id':
+            autocomplete.ListSelect2('jurisdiction-autocomplete')
         }
         exclude = []
-
