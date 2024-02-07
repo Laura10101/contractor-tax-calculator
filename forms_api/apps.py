6ebd2ecccffc59ac4f98cff50ed877a951a6e7c4
Laura10101 <laurajanefindley@gmail.com>
2024-02-07 10:10:04 +0000

    Fix linting issues for forms API apps.py

--- forms_api/apps.py
+++ forms_api/apps.py
@@ -1,7 +1,9 @@
+"""Defines the config class for the forms API."""
 from django.apps import AppConfig
 
 
 class FormsApiConfig(AppConfig):
+    """Defines the configuration for the forms API."""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'forms_api'
     verbose_name = 'Forms'
