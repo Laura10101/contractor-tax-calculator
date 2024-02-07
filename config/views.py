2b06291fd619ec08897a4e734437f51c81e53da2
Laura10101 <laurajanefindley@gmail.com>
2024-02-07 09:52:20 +0000

    Fix linting issues in config app views

--- config/views.py
+++ config/views.py
@@ -1,8 +1,13 @@
+"""Define views for the config app."""
+
 from django.shortcuts import render
 from django.contrib import admin
 
-# Create your views here.
+
 @admin.site.admin_view
 def config(request):
+    """Display the config app template."""
+    """This is a single page application so"""
+    """this view only needs to render the template"""
     template = 'config/config.html'
     return render(request, template)
